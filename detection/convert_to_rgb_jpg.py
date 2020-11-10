d4d6849774890920c3305232fd4fac3c42b09a83
pranathivemuri <pranathi93.vemuri@gmail.com>
2020-11-10 00:22:51 -0800

    add rpi detect video, rpi camera

---
+++ detection/convert_to_rgb_jpg.py
@@ -0,0 +1,27 @@
+import argparse
+import glob
+import os
+
+import cv2
+
+# Program to convert gray to rgb jpg images
+
+
+def convert_to_rgb_gray(image):
+  return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+
+def main():
+  parser = argparse.ArgumentParser(
+      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument('-o', '--output', required=True,
+                      help='File path of .tflite file.')
+  parser.add_argument('-i', '--input', required=True,
+                      help='File path of image to process.')
+  parser.add_argument('-f', '--format', type=str, default=".png",
+                      help='Format of image')
+  args = parser.parse_args()
+  for input_image in glob.glob(os.path.join(args.input, "*" + args.format)):
+  	cv2.imwrite(
+  	  os.path.join(args.output, os.path.basename(input_image)),
+  	  convert_to_rgb_gray(cv2.imread(input_image)))
