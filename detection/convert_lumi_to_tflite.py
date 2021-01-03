88fc51d0dc77d7bcc31f5fb6add67985d3537060
pranathivemuri <pranathi93.vemuri@gmail.com>
2021-01-02 16:26:31 -0800

    prin trainable vars

--- detection/convert_lumi_to_tflite.py
+++ detection/convert_lumi_to_tflite.py
@@ -1,17 +1,39 @@
 import argparse
 import os
+import re
 
 import tensorflow as tf
-
+from luminoth.models import get_model
+from luminoth.tools.checkpoint import get_checkpoint_config
+from luminoth.utils.training import get_optimizer
 # Program to convert lumi checkpoint to tflite model
 
 
-def convert_lumi_to_tflite(model, checkpoint, output_dir):
+def convert_lumi_to_tflite(model, config, checkpoint, output_dir):
     # path to the SavedModel directory
     tf.reset_default_graph()
     saver = tf.train.import_meta_graph(model)
     builder = tf.saved_model.builder.SavedModelBuilder(output_dir)
+    # Create custom init for slots in optimizer, as we don't save them to
+    # our checkpoints. An example of slots in an optimizer are the Momentum
+    # variables in MomentumOptimizer. We do this because slot variables can
+    # effectively duplicate the size of your checkpoint!
+    config = get_checkpoint_config(config)
+    model_class = get_model(config.model.type)
+    model = model_class(config)
+    trainable_vars = model.get_trainable_vars()
+    global_step = re.findall(r'[0-9]+', os.path.basename(checkpoint))
+    optimizer = get_optimizer(config.train, global_step)
+    slot_variables = [
+        tf.train.MomentumOptimizer.get_slot(var, name)
+        for name in optimizer.get_slot_names()
+        for var in trainable_vars
+    ]
+    slot_init = tf.variables_initializer(
+        slot_variables)
+
     with tf.Session() as sess:
+        sess.run(slot_init)
         # Restore variables from disk.
         saver.restore(sess, checkpoint)
         print("Model restored.")
