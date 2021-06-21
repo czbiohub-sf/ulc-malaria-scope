ffaaffeb55442cf40f85362e5cf5b5d6f8f12687
pranathivemuri <pranathi93.vemuri@gmail.com>
2021-06-20 21:35:50 -0700

    napari camera display works now

--- detection/detect_camera_napari_display.py
+++ detection/detect_camera_napari_display.py
@@ -28,13 +28,8 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 from PIL import ImageDraw
-
-
-import numpy as np
 from qtpy.QtCore import QThread
 import napari
-import time
-import cv2
 
 import detect
 import utils
@@ -44,20 +39,27 @@ from constants_ulc import (
     DEFAULT_CONFIDENCE,
     DEFAULT_INFERENCE_COUNT,
     DEFAULT_FILTER_AREA,
+    BOX_ANNOTATIONS,
 )
 
 
 class ThreadVideo(QThread):
-    def __init__(self, layer, shape_layer, videostream, interpreter):
+    def __init__(
+        self, layer, shape_layer, videostream, interpreter, output, debug, threshold
+    ):
         QThread.__init__(self)
         self.layer = layer
         self.videostream = videostream
         self.interpreter = interpreter
-        self.shapes_ = shape_layer
+        self.shapes = shape_layer
+        self.output = output
+        self.debug = debug
+        self.threshold = threshold
 
     def run(self):
+        frame_count = 0
         while True:
-            frame = videostream.read()
+            frame = self.videostream.read()
             image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             print(
                 "numpy image shape, size, sum {} {} {}".format(
@@ -66,27 +68,20 @@ class ThreadVideo(QThread):
             )
             image = Image.fromarray(image)
             scale = detect.set_input(
-                interpreter,
+                self.interpreter,
                 image.size,
                 lambda size: image.resize(size, Image.ANTIALIAS),
             )
-            for _ in range(count):
-                start = time.perf_counter()
-                interpreter.invoke()
-                inference_time = time.perf_counter() - start
-                objs = detect.get_output(interpreter, threshold, scale)
-                print("%.2f ms" % (inference_time * 1000))
+            start = time.perf_counter()
+            self.interpreter.invoke()
+            inference_time = time.perf_counter() - start
+            objs = detect.get_output(self.interpreter, self.threshold, scale)
+            print("%.2f ms" % (inference_time * 1000))
             input_image = "frame_{}.png".format(frame_count)
             print(input_image)
             numpy_image = np.array(image)
-            if filter_background_bboxes:
-                numpy_image = numpy_image[:, :, 0]
-                thresholded_image = np.zeros(
-                    (numpy_image.shape[0], numpy_image.shape[1]), dtype=np.uint8
-                )
-                thresh_value = 128
-                thresholded_image[numpy_image < thresh_value] = 1
-            filtered_objs = []
+            rects = []
+            labels_properties = []
             for obj in objs:
                 xmin, xmax, ymin, ymax = (
                     obj.bbox.xmin,
@@ -129,31 +124,32 @@ class ThreadVideo(QThread):
                             ignore_index=True,
                         )
                         filtered_objs.append(obj)
-            rects = []
-            for (xmin, ymin, xmax, ymax) in faces:
-                rects.append(np.array([[y, x], [y, x + w], [y + h, x + w], [y + h, x]]))
-            self.shapes_.data = rects
-            self.layer.data = rgb
-            frame_count += 1
-            if debug:
+                    x1 = xmin
+                    x2 = xmax
+                    y1 = ymin
+                    y2 = ymax
+                    label = labels.get(obj.id, obj.id)
+                    z = frame_count
+                    bbox_rect = np.array(
+                        [[z, y1, x1], [z, y2, x1], [z, y2, x2], [z, y1, x2]]
+                    )
+                    rects.append(bbox_rect)
+                    labels_properties.append(label)
+                self.shapes.data = rects
+                self.shapes.properties["box_label"] = np.array(
+                    labels_properties, dtype="<U32"
+                )
+                self.shapes.text.refresh_text(shapes_layer.properties)
+                self.layer.data = rgb
+                frame_count += 1
+            if self.debug:
                 image = image.convert("RGB")
-                image.save(os.path.join(os.path.abspath(output), input_image))
-                utils.draw_objects(ImageDraw.Draw(image), objs, labels)
+                image.save(os.path.join(os.path.abspath(self.output), input_image))
+                utils.draw_objects(ImageDraw.Draw(image), filtered_objs, labels)
         df.to_csv(os.path.join(os.path.abspath(output), "preds_val.csv"))
 
 
-def detect_stream(
-    model,
-    use_tpu,
-    labels,
-    threshold,
-    output,
-    count,
-    resolution,
-    debug,
-    area_filter,
-    filter_background_bboxes,
-):
+def detect_stream(model, use_tpu, labels, output, resolution, debug, threshold):
     labels = utils.load_labels(labels) if labels else {}
     utils.create_dir_if_not_exists(output)
     # Import TensorFlow libraries
@@ -196,7 +192,6 @@ def detect_stream(
     df = pd.DataFrame(columns=LUMI_CSV_COLUMNS)
 
     # Initialize frame count calculation
-    frame_count = 0
     print("----INFERENCE TIME----")
     with napari.gui_qt():
         # Grab frame from video stream
@@ -204,9 +199,22 @@ def detect_stream(
         # initiate viewer, image layer and shape layer
         viewer = napari.Viewer()
         layer = viewer.add_image(frame)
-        shape_layer = viewer.add_shapes([])
+        text_kwargs = {"text": "box_label", "size": 8, "color": "green"}
+        add_kwargs = dict(
+            face_color="black",
+            edge_color="box_label",
+            edge_width=3,
+            properties={"box_label": BOX_ANNOTATIONS},
+            ndim=3,
+            text=text_kwargs,
+            name="Shapes",
+            opacity=0.5,
+        )
+        shape_layer = viewer.add_shapes(None, **add_kwargs)
 
-        t = ThreadVideo(layer, shape_layer, videostream, casc)
+        t = ThreadVideo(
+            layer, shape_layer, videostream, interpreter, output, debug, threshold
+        )
         t.start()
 
 
@@ -234,6 +242,12 @@ def loop():
         help="File path of labels file.",
     )
     parser.add_argument(
+        "-o", "--output", help="File path for the result image with annotations"
+    )
+    parser.add_argument(
+        "--resolution", help="Desired webcam resolution in WxH", default="1280x720"
+    )
+    parser.add_argument(
         "-t",
         "--threshold",
         type=float,
@@ -241,32 +255,8 @@ def loop():
         help="Score threshold for detected objects.",
     )
     parser.add_argument(
-        "-o", "--output", help="File path for the result image with annotations"
-    )
-    parser.add_argument(
-        "-c",
-        "--count",
-        type=int,
-        default=DEFAULT_INFERENCE_COUNT,
-        help="Number of times to run inference",
-    )
-    parser.add_argument(
-        "--resolution", help="Desired webcam resolution in WxH", default="1280x720"
-    )
-    parser.add_argument(
         "--debug",
         help="Enable debug, saves individual frames if enabled",
-        action="store_true",
-    )
-    parser.add_argument(
-        "--area_filter",
-        help="Enable filtering bounding boxes of area",
-        type=int,
-        default=DEFAULT_FILTER_AREA,
-    )
-    parser.add_argument(
-        "--filter_background_bboxes",
-        help="Enable filtering bounding boxes that are in the background",
         action="store_true",
     )
 
@@ -276,13 +266,10 @@ def loop():
         args.model,
         args.edgetpu,
         args.labels,
-        args.threshold,
         args.output,
-        args.count,
         args.resolution,
         args.debug,
-        args.area_filter,
-        args.filter_background_bboxes,
+        args.threshold,
     )
 
 
