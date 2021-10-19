Switched from numpy to Welford's method to compute average update step for the fixed-number of frames background subtraction. Much faster, on the order of microseconds, as compared to numpy on the order of milliseconds.

ulc_mm_package/image_processing/patchy_background_subtraction.py
@@ -1,44 +1,50 @@
+""" PatchyBackgroundSubtraction - Find the average image background based on bounding boxes
+
+Find and average pixel values that lay outside bounding boxes across a series of image frames.
+
+#TODO
+Remove the dependence on the "BBox" object that is used for this project and just generalize to taking inputs to x/y arrays
+
+-- Important Links -- 
+Background:
+    
+"""
+
 # ========================= HEADER ===================================
 # title             :patchy_background_subtraction.py
 # description       :Find and average pixel values that lay outside bounding boxes across a series of image frames. #TODO make a better description
 # Main author       :Ilakkiyan Jeyakumar
 
 # ========================== IMPORTS ======================
+from typing import List
+from cv2 import subtract
 import numpy as np
-
 
 # ========================== CONSTANTS ======================
 INSIDE_BBOX_FLAG = 0
 
 class PatchyBackgroundSubtraction():
 
-    def _maskBoxedRegions(self, img_arr, bboxes):
+    def _maskBoxedRegions(self, img_arr: np.ndarray, xmin_vals: List[int], xmax_vals: List[int], ymin_vals: List[int], ymax_vals: List[int]):
         """Set the pixel values of regions inside the bounding boxes to -1 and return the image array.
         
         Parameters
         ----------
             - img_arr: numpy.ndarray
-            - bboxes: a list of BoundingBox (BBox) objects
-                These objects are defined under the /detection folder
+            - xmin_vals: List[int] - list of the minimum x-values for each bounding box
+            - xmax_vals: List[int] - list of the maximum x-values for each bounding box
+            - ymin_vals: List[int] - list of the minimum y-values for each bounding box
+            - ymax_vals: List[int] - list of the maximum y-values for each bounding box
         """
 
-
-        # Extract relevant parameters from the BBox class
-        xmin_vals = np.asarray([bbox.xmin for bbox in bboxes])
-        xmax_vals = np.asarray([bbox.xmax for bbox in bboxes])
-        ymin_vals = np.asarray([bbox.ymin for bbox in bboxes])
-        ymax_vals = np.asarray([bbox.ymax for bbox in bboxes])
-
         for xmin, ymin, xmax, ymax in zip(xmin_vals, ymin_vals, xmax_vals, ymax_vals):
-            # img_arr[xmin:xmax, ymin:ymax] = INSIDE_BBOX_FLAG
-            # TODO fix the line below
+            # Numpy indexing is [row (y), column (x)]
             img_arr[ymin:ymax, xmin:xmax] = INSIDE_BBOX_FLAG
 
         return img_arr
         
-    def getBackgroundAverageArray(self):
+    def getBackgroundAverageArray(self) -> np.ndarray:
         """Get the averaged background value for each pixel based on the past N frames."""
-        self._updateBackgroundAverage()
         return self._backgroundAverageArray
 
 class PatchyBackgroundSubtractionFixedNum(PatchyBackgroundSubtraction):
@@ -62,8 +68,9 @@ class PatchyBackgroundSubtractionFixedNum(PatchyBackgroundSubtraction):
         self._oldest_frame_ptr = 0
         self._num_frames_in_memory = num_frames_in_memory
         self._backgroundAverageArray = np.zeros((img_height, img_width))
+        self.img_counter = np.zeros((img_height, img_width))
 
-    def addImage(self, img_arr, bboxes):
+    def addImage(self, img_arr, xmin_vals, xmax_vals, ymin_vals, ymax_vals):
         """Add an image to be included in the frame storage. 
         
         If the frame storage is full (i.e already contains `num_frames_in_memory` images), 
@@ -74,7 +81,8 @@ class PatchyBackgroundSubtractionFixedNum(PatchyBackgroundSubtraction):
         img_arr : an array with the same dimensions specified during class instantiation
         bboxes : list of BoundingBox objects
         """
-        masked_img = self._maskBoxedRegions(img_arr, bboxes)
+        masked_img = self._maskBoxedRegions(img_arr, xmin_vals, xmax_vals, ymin_vals, ymax_vals)
+        self._updateBackgroundAverage(masked_img)
         self._addImageToMemory(masked_img)
 
     def _addImageToMemory(self, img_arr):
@@ -87,18 +95,26 @@ class PatchyBackgroundSubtractionFixedNum(PatchyBackgroundSubtraction):
         self.frame_storage[:, :, self._oldest_frame_ptr] = img_arr
         self._oldest_frame_ptr = (self._oldest_frame_ptr + 1) % self._num_frames_in_memory
 
-    def _updateBackgroundAverage(self):
+    def _updateBackgroundAverage(self, masked_img):
         """Computes the average background value for each pixel.
         
-        The frame_storage array is an array with shape (height, width, num_frames). Below we take the average for each
-        pixel along the num_frames axis. A mask is applied so that pixels which lie inside a bounding box for a particular frame
-        are excluded from the averaging calculation. Side note, np.ma.average is used instead of the usual np.average because
-        np.ma.average elegantly deals with the (rare) case where all the values along an axis are masked out. The normal np.average 
-        would raise a ZeroDivisionError. For more, see: 
-            - https://numpy.org/doc/stable/reference/generated/numpy.ma.average.html#numpy.ma.average
-            - https://numpy.org/doc/stable/reference/generated/numpy.ma.average.html#numpy.ma.average
+        Uses Welford's method to compute the updated average (single-pass, so no need to recompute everything). The oldest frame is removed from the average,
+        and the new frame is added. Benchmarking on my computer (Macbook Pro 13) this takes ~5us (previously used numpy and that took ~300ms for a 
+        (800, 600, 100) frame array).
         """
-        self._backgroundAverageArray = np.ma.average(self.frame_storage, axis=2, weights=(self.frame_storage != INSIDE_BBOX_FLAG)).filled(0)
+        # Remove the oldest frame from the average ( avg_prev = avg_curr - (avg_curr - old_frame) / N )
+        oldest_frame = self.frame_storage[:, :, self._oldest_frame_ptr]
+        self.img_counter[oldest_frame != INSIDE_BBOX_FLAG] -= 1
+        subtract_step = np.divide((oldest_frame - self._backgroundAverageArray), self.img_counter, where=self.img_counter != 0)
+        subtract_step[oldest_frame == INSIDE_BBOX_FLAG] = 0
+        self._backgroundAverageArray = self._backgroundAverageArray - subtract_step
+
+        # Add the newest frame ( avg_new = avg_curr + (new_frame + avg_curr) / (N+1) )
+        self.img_counter[masked_img != INSIDE_BBOX_FLAG] += 1
+        add_step = np.divide((masked_img - self._backgroundAverageArray), self.img_counter, where=self.img_counter != 0)
+        add_step[masked_img == INSIDE_BBOX_FLAG] = 0
+        self._backgroundAverageArray = self._backgroundAverageArray + add_step
+        
 
 class PatchyBackgroundSubtractionContinuous(PatchyBackgroundSubtraction):
     def __init__(self, img_width: int=0, img_height: int=0):
@@ -112,9 +128,9 @@ class PatchyBackgroundSubtractionContinuous(PatchyBackgroundSubtraction):
             y dimension of the images to be stored
         """
         self._backgroundAverageArray = np.zeros((img_height, img_width))
-        self.img_counter = 1
+        self.img_counter = np.zeros((img_height, img_width))
 
-    def addImage(self, img_arr, bboxes):
+    def addImage(self, img_arr, xmin_vals, xmax_vals, ymin_vals, ymax_vals):
         """Add an image to be included in the average background average. 
 
         Parameters
@@ -122,14 +138,16 @@ class PatchyBackgroundSubtractionContinuous(PatchyBackgroundSubtraction):
         img_arr : an array with the same dimensions specified during class instantiation
         bboxes : list of BoundingBox objects
         """
-        masked_img = self._maskBoxedRegions(img_arr, bboxes)
+        masked_img = self._maskBoxedRegions(img_arr, xmin_vals, xmax_vals, ymin_vals, ymax_vals)
         self._updateBackgroundAverage(masked_img)
 
-    def getBackgroundAverageArray(self):
-        """Get the averaged background value for each pixel based on the past N frames."""
-        return self._backgroundAverageArray
-
     def _updateBackgroundAverage(self, masked_img):
-        """Updates the average background value for each pixel."""
-        self.img_counter += 1
-        self._backgroundAverageArray = self._backgroundAverageArray + (masked_img - self._backgroundAverageArray) / self.img_counter
+        """Updates the average background value for each pixel.
+        
+        The denominator (when calculating the mean) is only incremented for 
+        those pixels that were actually part of the background.
+        """
+        self.img_counter[masked_img != INSIDE_BBOX_FLAG] += 1
+        update_step = np.divide((masked_img - self._backgroundAverageArray), self.img_counter, where=self.img_counter != 0)
+        update_step[masked_img == INSIDE_BBOX_FLAG] = 0
+        self._backgroundAverageArray = self._backgroundAverageArray + update_step