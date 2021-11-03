457bb0c4b16a4cc110fe20ac5ae77c9c8b3238d0
i-jey <ilakkiyan.jeyakumar@iJeyakumar-MBP.local>
2021-11-02 17:32:51 -0700

    Added a more descriptive header with details of the encoder used. Also added functionality so that the encoder can take in a callable which can triggered on interrupt.

--- ulc_mm_package/hardware/encoder.py
+++ ulc_mm_package/hardware/encoder.py
@@ -1,9 +1,15 @@
-# ========================= HEADER ===================================
-# title             :encoder.py
-# description       :An Encoder class using a binary lookup table to quickly determine encoder clicks + direction
-# Main author       :Ilakkiyan Jeyakumar
+""" Adafruit Rotary Encoder (PEC11-4215F-S24 encoder w/ 6mm knob)
 
-# ========================== IMPORTS ======================
+-- Important Links -- 
+Adafruit Product Page:
+    https://www.adafruit.com/product/377
+Encoder Datasheet:
+    https://cdn-shop.adafruit.com/datasheets/pec11.pdf
+Knob Datasheet (mechanical dimensions):
+    https://cdn-shop.adafruit.com/datasheets/EPD-200732.pdf
+"""
+
+from typing import Callable
 import pigpio
 
 '''The lookup table maps the state transition of the two encoder pins:
@@ -28,36 +34,61 @@ increment the encoder count.
 
 # ==================== Main class ===============================
 class Encoder():
-    def __init__(self, pin_a: int, pin_b: int, pi: pigpio.pi=None):
+    def __init__(self, pin_a: int, pin_b: int, pi: pigpio.pi=None, custom_callback: Callable = None):
         self.pin_a = pin_a
         self.pin_b = pin_b
         self.lookup_table = [0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0]
         self.encoder_value = 0b0000
         self.encoder_count = 0
         self._pi = pi if pi != None else pigpio.pi()
+        self.custom_callback = custom_callback
+        self.last_dir = 0
 
         # Set up GPIO and callbacks
         self._pi.set_mode(self.pin_a, pigpio.INPUT)
+        self._pi.set_pull_up_down(self.pin_a, pigpio.PUD_DOWN)
         self._pi.set_mode(self.pin_b, pigpio.INPUT)
-        self._pi.callback(self.pin_a, pigpio.RISING_EDGE, self.encoderISR)
-        self._pi.callback(self.pin_b, pigpio.RISING_EDGE, self.encoderISR)
+        self._pi.set_pull_up_down(self.pin_b, pigpio.PUD_DOWN)
+        self.enableEncoder()
+        self.enabled = True
+
+    def enableEncoder(self):
+        self.cb1 = self._pi.callback(self.pin_a, pigpio.EITHER_EDGE, self.encoderISR)
+        self.cb2 = self._pi.callback(self.pin_b, pigpio.EITHER_EDGE, self.encoderISR)
+
+    def disableEncoder(self):
+        self.cb1.cancel()
+        self.cb2.cancel()
+
+    def isEnabled(self):
+        return self.enabled
 
     def encoderISR(self, *args):
         # Read the two input pins and convert it to a 2-bit binary (i.e 00 / 10 / 01 / 11)
-        val = self._pi.read(self.pin_a) << 1 | self._pi.read(self.pin_b)
+        a = self._pi.read(self.pin_a)
+        b = self._pi.read(self.pin_b)
+        val = a << 1 | b
 
         # Left shift the encoder value and OR with the current readout  
         # (OLD, NEW) --> (NEW, NEW') (the previous new becomes old and we add
         # in the current readout into the 2 least significant bits)
         # Lastly bitwise AND with 15 (1111) to retain only last 4 bits
-        self.encoder_value = ((self.encoder_value << 2) | val) & 15
-
+        self.encoder_value = ((self.encoder_value << 2) | val) & 0b1111
+        
         # Finally use the lookup table to figure out how the encoder has moved
         # and increment
-        self.encoder_count += self.lookup_table[self.encoder_value]
+        increment = self.lookup_table[self.encoder_value]
+        self.encoder_count += increment
 
-    def getCount(self):
+        # Call the custom callback with the movement direction (1, 0, -1)
+        if self.encoder_count % 4 == 0 and self.custom_callback:
+            self.custom_callback(increment)
+
+    def getRawCount(self):
         return self.encoder_count
+    
+    def getDetentCount(self):
+        return self.encoder_count // 4
 
     def resetCount(self):
         self.encoder_count = 0
