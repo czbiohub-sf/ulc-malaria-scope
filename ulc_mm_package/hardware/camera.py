6df842ed827696d70d3bc60f1dee5abbccc14fd5
i-jey <ilakkiyan.jeyakumar@iJeyakumar-MBP.local>
2021-11-03 12:58:59 -0700

    Wrapped init in try except.

--- ulc_mm_package/hardware/camera.py
+++ ulc_mm_package/hardware/camera.py
@@ -15,6 +15,11 @@ from py_cameras import Basler, GrabStrategy
 # ------ CONSTANTS ------ #
 _DEFAULT_EXPOSURE_MS = 1
 
+class CameraError(Exception):
+    """Base class for catching camera errors."""
+    # Note this is temporary until the pyCameras improved exception-handling PR is merged.
+    # Once that is merged, we can simply raise the PyCameras error.
+
 class StoppableThread(threading.Thread):
     """Thread class which can be stopped"""
     def __init__(self, *args, **kwargs):
@@ -32,15 +37,18 @@ class ULCMM_Camera(Basler):
     """Extends the Basler camera class from pycameras and makes a few ULCMM specific configuration changes."""
 
     def __init__(self):
-        super().__init__()
-    
-        # 2x2 binning w/ averaging (https://docs.baslerweb.com/binning)
-        # Note that setting the binning mode to "Sum" saturates the values (i.e if
-        # the pixel mode is 8-bit (0-256), summing does NOT increase the maximum value to 512)
-        self.setBinning(bin_factor=2, mode="Average")
-        self.camera.PixelFormat.SetValue("Mono8")
-        self.exposureTime_ms = _DEFAULT_EXPOSURE_MS
-        self.grabStrategy = GrabStrategy.LATEST_IMAGE_ONLY
+        try:
+            super().__init__()
+
+            # 2x2 binning w/ averaging (https://docs.baslerweb.com/binning)
+            # Note that setting the binning mode to "Sum" saturates the values (i.e if
+            # the pixel mode is 8-bit (0-256), summing does NOT increase the maximum value to 512)
+            self.setBinning(bin_factor=2, mode="Average")
+            self.camera.PixelFormat.SetValue("Mono8")
+            self.exposureTime_ms = _DEFAULT_EXPOSURE_MS
+            self.grabStrategy = GrabStrategy.LATEST_IMAGE_ONLY
+        except Exception:
+            raise CameraError("Camera could not be instantiated.")
 
     def yieldImages(self):
         return super().yieldImages(self.grabStrategy)
