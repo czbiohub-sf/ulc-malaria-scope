a20a01466cd7dade6c582415290d93ccb3eaa697
i-jey <ilakkiyan.jeyakumar@iJeyakumar-MBP.local>
2022-05-20 16:22:28 -0700

    FlowRateEstimator estimator using cross-correlation

--- ulc_mm_package/image_processing/flowrate.py
+++ ulc_mm_package/image_processing/flowrate.py
@@ -1,9 +1,104 @@
+from re import L
 import numpy as np
+import cv2
 
-X_TOL = 15
+class FlowRateEstimatorError(Exception):
+    """Base class for catching all pressure control related errors."""
+    pass
+
+class FlowRateEstimator:
+
+    def __init__(self, img_width: int=800, img_height: int=600, num_image_pairs: int=30):
+        """A class for estimating the flow rate of cells.
+        The class holds two images at a time in `frame_storage`. To use this class,
+        a user needs only to provide a single image at a time. Every two images, 
+        the class calculates the displacement them.
+
+        Eaxmple usage (pseudocode)
+        ---------------------------
+
+            fre = FlowRateEstimator()
+
+            while im in camera..yieldImages():
+                fre.addImageAndCalculatePair()
+
+            if fre.isFull():
+                dx, dy = fre.getAverageAndReset()
+
+        Parameters
+        ----------
+        img_width : int=800 (default)
+            x dimension of the images to be stored
+        img_height : int=600 (default)
+            y dimension of the images to be stored
+        num_image_pairs: int=60 (default)
+            The number of image pairs for which to calculate flow rate values - this number
+            sets the size of the displacement arrays determines when `isFull` returns True.
+        """
+
+        self.dx = [0] * num_image_pairs
+        self.dy = [0] * num_image_pairs
+        self.frame_storage = np.zeros((img_height, img_width, 2), dtype=np.uint8)
+        self._frame_counter = 0
+        self._calc_idx = 0
+
+    def _getAverageDisplacements(self):
+        """Internal function - return the mean of the dx and dy displacement arrays"""
+        return np.average(self.dx), np.average(self.dy)
+
+    def getAverageAndReset(self):
+        """Get the average of all the values in the displacement arrays and reset the calculation counter.
+        A user caling this function should only call it once `isFull` returns True.
+
+        Once `_calc_idx` is reset, it will overwrite previous displacement values stored in dx and dy.
+        The `isFull` function will return false until `num_image_pairs' calculations have been done again.
+        """
+        self._calc_idx = 0
+        return self._getAverageDisplacements()
+
+    def _addImage(self, img_arr: np.ndarray):
+        """Internal function - add image to the storage.
+
+        The oldest image is overwritten and the pointer is incremented.
+        """
+        self.frame_storage[:, :, self._frame_counter] = img_arr
+        self._frame_counter = (self._frame_counter + 1) % 2
+
+    def _calculatePairDisplacement(self):
+        if self._frame_counter == 0 and not self.isFull():
+            dx, dy = getFlowrateWithCrossCorrelation(self.frame_storage[:, :, 0], self.frame_storage[:, :, 1])
+
+            if self.isValidDisplacement(dx, dy):
+                self.dx[self._calc_idx], self.dy[self._calc_idx] = dx, dy
+                self._calc_idx += 1
+
+    def isValidDisplacement(self, dx, dy):
+        """A function to check for valid displacement values.
+
+        For now, this function is very simple (i.e only checking for non-negative y-displacement).
+        However, if additional logic needs to be implemented, it can go here.
+        """
+        if dy > 0:
+            return True
+
+    def isFull(self):
+        """Returns True when the calculation (dx, dy) arrays are full.
+
+        The size of `dx` is set during class initialization by the `num_image_pairs` parameter.
+        """
+        return self._calc_idx >= len(self.dx)
+
+    def addImageAndCalculatePair(self, img: np.ndarray):
+        """A convenience function to add an image and perform a displacement calculation.
+
+        `_calculatePairDisplacement` only runs if two images have been past since the last calculation.
+        (I.e there is no chance that two non-temporally-sequential images will be run together).
+        """
+        self._addImage(img)
+        self._calculatePairDisplacement()
 
 
-def getFlowRateInPixels(prev_bboxes, curr_bboxes):
+def getFlowrateInPixelsFromBBoxes(prev_bboxes, curr_bboxes, x_tol: int=15):
     """
     Returns the flow rate in number of pixels based on the previous and current frame bounding boxes
 
@@ -32,15 +127,15 @@ def getFlowRateInPixels(prev_bboxes, curr_bboxes):
 
     for i, (c_xmin, c_xmax) in enumerate(zip(curr_xmin, curr_xmax)):
         # If there are multiple bounding boxes in the current frame in the same vertical region
-        if np.count_nonzero(np.isclose(curr_xmin, c_xmin, atol=X_TOL)) > 1:
+        if np.count_nonzero(np.isclose(curr_xmin, c_xmin, atol=x_tol)) > 1:
             continue
 
         # Find the bounding box in the previous frame corresponding to the current box
         index_xmin_in_prev_frame = np.argwhere(
-            np.isclose(prev_xmin, c_xmin, atol=X_TOL) == True
+            np.isclose(prev_xmin, c_xmin, atol=x_tol) == True
         )
         index_xmax_in_prev_frame = np.argwhere(
-            np.isclose(prev_xmax, c_xmax, atol=X_TOL) == True
+            np.isclose(prev_xmax, c_xmax, atol=x_tol) == True
         )
 
         # Ensure there is only one bounding box in the previous frame which corresponds to the current
@@ -56,7 +151,119 @@ def getFlowRateInPixels(prev_bboxes, curr_bboxes):
 
     return np.average(all_displacements)
 
-
 def convertPixelToDistance(pixels):
     # TODO
     return 1
+
+def downSampleImage(img: np.ndarray, scale_factor: int):
+    """Downsamples an image by `scale_factor`"""
+    h, w = img.shape
+    return cv2.resize(img, (w // scale_factor, h // scale_factor))
+
+def getTemplateRegion(img: np.ndarray, x1_perc: float=0.05, y1_perc: float=0.05, x2_perc: float=0.45, y2_perc: float=0.85):
+    """Returns a subregion of the image provided.
+    The start and end positions are to be given as percentages of the image's shape.
+
+    Parameters
+    ----------
+        img : np.ndarray
+            An image (i.e a numpy array)
+        x1_perc : float
+            What percentage of the image the start of the subregion should begin at.
+            For example, x1_perc=0.05 of an input image with shape (600, 800) would mean the
+            subregion would begin at 0.05*600=30.
+        y1_perc: float
+            See x1_perc
+        x2_perc: float
+            See x1_perc
+        y2_perc: float
+            See x1_perc
+
+    Returns
+    -------
+        np.ndarray:
+            subregion of the input array
+        int:
+            x_offset (i.e x start of the subregion)
+        int:
+            y_offset (i.e y start of the subregion)
+    """
+
+    h, w = img.shape
+    xs, xf = int(x1_perc*w), int(x2_perc*w)
+    ys, yf = int(y1_perc*h), int(y2_perc*h)
+
+    return img[ys:yf, xs:xf], (xs, xf), (ys, yf)
+
+def getFlowrateWithCrossCorrelation(prev_img: np.ndarray, next_img: np.ndarray, scale_factor: int=10, debug: bool=False):
+    """Find the displacement of a subregion of an image with another, temporally adjacent, image
+    
+    Parameters
+    ----------
+        img : np.ndarray
+            An image (i.e a numpy array)
+        x1_perc : float
+            What percentage of the image the start of the subregion should begin at.
+            For example, x1_perc=0.05 of an input image with shape (600, 800) would mean the 
+            subregion would begin at 0.05*600=30.
+        y1_perc: float
+            See x1_perc
+        x2_perc: float
+            See x1_perc
+        y2_perc: float
+            See x1_perc
+
+    Returns
+    -------
+        int:
+            dx: displacement in x
+        int:
+            dy: displacement in y
+    """
+    im1_ds, im2_ds = downSampleImage(prev_img, scale_factor), downSampleImage(next_img, scale_factor)
+
+    # Select the subregion within the first image by defining which quantiles to use
+    im1_ds_subregion, x_offset, y_offset = getTemplateRegion(im1_ds, 0.05, 0.05, 0.85, 0.45)
+
+    # Run a normalized cross correlation between the image to search and subregion
+    template_result = cv2.matchTemplate(im2_ds, im1_ds_subregion, cv2.TM_CCOEFF_NORMED)
+
+    # Find the point with the maximum value (i.e highest match) and caclulate the displacement
+    _, _, _, max_loc = cv2.minMaxLoc(template_result)
+    dx, dy = max_loc[0] - x_offset[0], max_loc[1] - y_offset[0]
+
+    # If debug mode is on, run `plot_cc` which saves images of the cross-correlation calculation.
+    if debug:
+        plot_cc(im1_ds, im2_ds, im1_ds_subregion, template_result, (x_offset[0], y_offset[0]), (x_offset[1], y_offset[1]), max_loc[0], max_loc[1], dx, dy)
+
+    return dx, dy
+
+
+def plot_cc(im1, im2, im1_subregion, template_result, xy1, xy2, max_x, max_y, dx, dy):
+    """A function for debugging and visualizing the cross-correlation 
+    displacement calculation.
+    
+    This function produces a 2x2 plot of:
+        Top-left: The first image
+        Bottom-left: The second image
+        Top-right: A close-up of the subregion of interest in the first image
+        Bottom-right: A close-up of the subregion with the closest match in the second image
+    """
+    import matplotlib.pyplot as plt
+
+    h, w = im1_subregion.shape
+    im1_subregion = im1_subregion.copy()
+    im2_subregion = im2[max_y:max_y+h, max_x:max_x+w].copy()
+    im1 = cv2.rectangle(im1, xy1, xy2, 255, 1)
+    im2 = cv2.rectangle(im2, (max_x, max_y), (max_x+w, max_y+h), 255, 1)
+
+    fig, ax = plt.subplots(3, 2, figsize=(10, 7))
+    ax[0, 0].imshow(im1, cmap='gray')
+    ax[0, 1].imshow(im1_subregion, cmap='gray')
+    ax[1, 0].imshow(im2, cmap='gray')
+    ax[1, 1].imshow(im2_subregion, cmap='gray')
+    ax[1, 1].text(0, 0, f"{dx, dy}", bbox={'facecolor': 'white', 'pad': 2})
+    ax[2, 0].imshow(template_result)
+    plt.show(block=False)
+    plt.pause(0.01)
+    plt.close()
