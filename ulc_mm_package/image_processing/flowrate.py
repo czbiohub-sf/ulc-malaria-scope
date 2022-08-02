2ff2c7a26c32d1ba9e04c5b739ccf668455a1854
i-jey <ilakkiyan.jeyakumar@iJeyakumar-MBP.local>
2022-08-02 00:26:16 -0700

    deprecated flowrate code

--- ulc_mm_package/image_processing/flowrate.py
+++ ulc_mm_package/image_processing/flowrate.py
@@ -137,64 +137,6 @@ class FlowRateEstimator:
         self._addImage(img, timestamp)
         self._calculatePairDisplacement()
 
-
-def getFlowrateInPixelsFromBBoxes(prev_bboxes, curr_bboxes, x_tol: int=15):
-    """
-    Returns the flow rate in number of pixels based on the previous and current frame bounding boxes
-
-    Parameters
-    ----------
-    prev_bboxes : List[BBox]
-        A list of BBox objects from the previous frame
-    curr_bboxes : List[BBox]
-        A list of BBox objects from the current frame
-
-    Returns
-    -------
-    int:
-        An integer representing the average bounding box displacements found between the previous and current frame
-
-    """
-    # Extract relevant parameters from the BBox class
-    prev_xmin = np.asarray([bbox.xmin for bbox in prev_bboxes])
-    prev_ymin = np.asarray([bbox.ymin for bbox in prev_bboxes])
-    prev_xmax = np.asarray([bbox.xmax for bbox in prev_bboxes])
-    curr_xmin = np.asarray([bbox.xmin for bbox in curr_bboxes])
-    curr_ymin = np.asarray([bbox.ymin for bbox in curr_bboxes])
-    curr_xmax = np.asarray([bbox.xmax for bbox in curr_bboxes])
-
-    all_displacements = []
-
-    for i, (c_xmin, c_xmax) in enumerate(zip(curr_xmin, curr_xmax)):
-        # If there are multiple bounding boxes in the current frame in the same vertical region
-        if np.count_nonzero(np.isclose(curr_xmin, c_xmin, atol=x_tol)) > 1:
-            continue
-
-        # Find the bounding box in the previous frame corresponding to the current box
-        index_xmin_in_prev_frame = np.argwhere(
-            np.isclose(prev_xmin, c_xmin, atol=x_tol) == True
-        )
-        index_xmax_in_prev_frame = np.argwhere(
-            np.isclose(prev_xmax, c_xmax, atol=x_tol) == True
-        )
-
-        # Ensure there is only one bounding box in the previous frame which corresponds to the current
-        if len(index_xmin_in_prev_frame > 0) and len(index_xmax_in_prev_frame > 0):
-            if len(index_xmin_in_prev_frame[0]) == 1:
-                # Find the y displacement
-                c_ymin = curr_ymin[i]
-                p_ymin = prev_ymin[index_xmin_in_prev_frame[0][0]]
-
-                if p_ymin < c_ymin:
-                    pixel_displacement = c_ymin - p_ymin
-                    all_displacements.append(pixel_displacement)
-
-    return np.average(all_displacements)
-
-def convertPixelToDistance(pixels):
-    # TODO
-    return 1
-
 def downSampleImage(img: np.ndarray, scale_factor: int):
     """Downsamples an image by `scale_factor`"""
     h, w = img.shape
