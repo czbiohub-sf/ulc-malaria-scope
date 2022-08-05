5478345750921d881dc66c2db1190426bab1a54a
Axel Jacobsen <axelnj44@gmail.com>
2022-08-04 17:09:30 -0700

    generalize and update NCS Model

---
+++ ulc_mm_package/neural_nets/NCSModel.py
@@ -0,0 +1,142 @@
+#! /usr/bin/env python3
+
+import cv2
+import sys
+import time
+import numpy as np
+
+from enum import Enum
+from pathlib import Path
+
+from typing import Any, Callable
+
+from openvino.preprocess import PrePostProcessor, ResizeAlgorithm
+from openvino.runtime import Core, Layout, Type, InferRequest, AsyncInferQueue
+
+
+""" TODOs
+- All the performance things!
+- Create ABC for Autofocus, obj detect
+- best place to put autofocus.xml && autofocus.bin? root dir?
+
+See https://github.com/czbiohub/ulc-malaria-autofocus#openvino-performance-optimizations for
+more performance things I want to do
+"""
+
+
+class OptimizationHint(Enum):
+    LATENCY = 1
+    THROUGHPUT = 2
+
+
+class NCSModel:
+    """
+    Neural Compute Stick 2 Model
+
+    Allows you to run a model (defined by an intel intermediate representation of your
+    model, e.g. model.xml & model.bin) on the neural compute stick
+
+    Very early in development!
+
+
+    Usage:
+        >>> A = NCSModel("model.xml")  # the model.bin file should be in same dir
+        >>> img = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
+        >>> A(img)
+        <steps from 0!>
+    """
+
+    def __init__(
+        self,
+        model_path: str,
+        optimization_hint: OptimizationHint = OptimizationHint.THROUGHPUT,
+    ):
+        """
+        params:
+            model_path: path to the 'xml' file
+        """
+        self.device_name = "MYRIAD"
+        self.core = Core()
+        self.model = self._compile_model(model_path, optimization_hint)
+        self.asyn_infer_queue = AsyncInferQueue(self.model)
+        self.asyn_infer_queue.set_callback(self._default_callback)
+        self.asyn_results = []
+
+    def _compile_model(self, model_path, perf_hint: OptimizationHint):
+        model = self.core.read_model(model_path)
+
+        input_tensor_shape = (1, 600, 800, 1)
+
+        # https://docs.openvino.ai/latest/openvino_docs_OV_UG_Preprocessing_Details.html#resize-image
+        ppp = PrePostProcessor(model)
+        ppp.input().tensor().set_shape(input_tensor_shape).set_element_type(
+            Type.u8
+        ).set_layout(Layout("NHWC"))
+        ppp.input().model().set_layout(Layout("NCHW"))
+        ppp.input().preprocess().resize(ResizeAlgorithm.RESIZE_LINEAR)
+        ppp.output().tensor().set_element_type(Type.f32)
+        model = ppp.build()
+
+        compiled_model = self.core.compile_model(
+            model, self.device_name, {"PERFORMANCE_HINT": perf_hint.name}
+        )
+
+        return compiled_model
+
+    def _default_callback(self, infer_request: InferRequest, userdata) -> None:
+        self.asyn_results.append(infer_request.output_tensors[0].data)
+
+    def syn(self, input_img):
+        """input_img is 2d array (i.e. grayscale img)"""
+        input_tensor = [np.expand_dims(input_img, (0, 3))]
+        results = self.model(input_tensor)
+        return list(results.values())
+
+    def asyn(
+        self,
+        input_img: list,
+    ) -> None:
+        input_tensor = [np.expand_dims(input_img, (0, 3))]
+        self.asyn_infer_queue.start_async(input_tensor)
+
+    def get_asyn_results(self):
+        res = self.asyn_results
+        self.asyn_results = []
+        return res
+
+
+if __name__ == "__main__":
+    def _asyn_calling(model, images):
+        print("starting asyncing")
+        t0 = time.perf_counter()
+        for image in images:
+            model.asyn(image)
+        model.asyn_infer_queue.wait_all()
+        t1 = time.perf_counter()
+        print(f"Total runtime {t1 - t0}")
+        print(f"Throughput {len(images) / (t1 - t0)} FPS")
+
+    def _syn_calling(model, images):
+        print("starting syncing")
+        times = []
+        for image in images:
+            t0 = time.perf_counter()
+            model.syn(image)
+            t1 = time.perf_counter()
+            times.append(t1 - t0)
+        ts = np.asarray(times)
+        print(
+            f"Total runtime {sum(ts)} \t mean inference {ts.mean()} \t std dev {ts.std()}"
+        )
+        print(f"Throughput {len(images) / sum(ts)} FPS")
+
+    image_path = Path(sys.argv[1])
+    if image_path.is_file():
+        images = [cv2.imread(sys.argv[1], cv2.IMREAD_GRAYSCALE)]
+    else:
+        sorted_fnames = sorted([str(p) for p in image_path.glob("*.tiff")])
+        images = [cv2.imread(fname, cv2.IMREAD_GRAYSCALE) for fname in sorted_fnames]
+
+    A = NCSModel("autofocus.xml", OptimizationHint.THROUGHPUT)
+    _syn_calling(A, images)
+    _asyn_calling(A, images)
