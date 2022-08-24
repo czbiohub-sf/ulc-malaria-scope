4abf1a5f7c54fab5223527055c2ebec930be95bb
i-jey <ilakkiyan.jeyakumar@iJeyakumar-MBP.local>
2022-08-24 10:19:56 -0700

    add AVT camera to imports

--- ulc_mm_package/hardware/hardware_modules.py
+++ ulc_mm_package/hardware/hardware_modules.py
@@ -1,4 +1,4 @@
-from ulc_mm_package.hardware.camera import BaslerCamera, CameraError
+from ulc_mm_package.hardware.camera import BaslerCamera, AVTCamera, CameraError
 from ulc_mm_package.hardware.motorcontroller import (
     DRV8825Nema,
     Direction,
