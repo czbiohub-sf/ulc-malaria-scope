376de59adcdbaf31e4e11d6f17aa1c70f8a18522
i-jey <ilakkiyan.jeyakumar@iJeyakumar-MBP.local>
2022-08-24 10:56:01 -0700

    missed a 0 - fixed devicelinkthroughput

--- ulc_mm_package/hardware/hardware_constants.py
+++ ulc_mm_package/hardware/hardware_constants.py
@@ -4,7 +4,7 @@ BOARD_STATUS_INDICATOR = 4
 
 # ================ Camera constants ================ #
 DEFAULT_EXPOSURE_MS = 0.5
-DEVICELINK_THROUGHPUT = 10000000
+DEVICELINK_THROUGHPUT = 100000000
 
 # ================ Motor controller constants ================ #
 FULL_STEP_TO_TRAVEL_DIST_UM = 0.56
