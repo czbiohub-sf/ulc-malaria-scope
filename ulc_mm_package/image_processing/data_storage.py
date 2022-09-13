2d4b5eb2bcc055cdc918e55a8135b1486952ecac
i-jey <ilakkiyan.jeyakumar@iJeyakumar-MBP.local>
2022-09-13 11:25:02 -0400

    DataStorage class: comments, single image write, metadata files and storing keys in the constants file.

--- ulc_mm_package/image_processing/data_storage.py
+++ ulc_mm_package/image_processing/data_storage.py
@@ -4,8 +4,10 @@ import csv
 from typing import Dict, List
 
 import numpy as np
+import cv2
 
 from ulc_mm_package.image_processing.zarrwriter import ZarrWriter
+from ulc_mm_package.image_processing.processing_constants import EXPERIMENT_METADATA_KEYS, PER_IMAGE_METADATA_KEYS
 
 class DataStorageError(Exception):
     pass
@@ -27,18 +29,27 @@ class DataStorage:
         except FileNotFoundError as e:
             raise DataStorageError(f"DataStorageError - Unable to make top-level directory: {e}")
 
-    def setMetadataKeys(self, keys: List[str]):
-        self.md_keys = keys
+    def createNewExperiment(self, custom_experiment_name: str, experiment_initialization_metdata: Dict=EXPERIMENT_METADATA_KEYS, per_image_metadata: Dict=PER_IMAGE_METADATA_KEYS):
+        """Create the storage files for a new experiment (Zarr storage, metadata .csv files)
 
-    def createNewExperiment(self, custom_experiment_name: str, md_keys: List[str]):
+        Parameters
+        ----------
+        custom_experiment_name: str
+            Appended to the end of the filename. Can also pass in an empty string ("") if no experiment name is needed.
+
+        experiment_initialization_metadata: Dict [str : val]
+            A dictionary of the experiment initialization parameters.
+
+        per_image_metadata: Dict [str : val]
+            A dictionary of the metadata to be stored on a per-image basis. Since this is just for initialization,
+            the dictionary does not necessarily need to have any values - this function just needs to keys in order to
+            create the `.csv` file.
+        """
+
         if self.main_dir == None:
             self.createTopLevelFolder()
-        self.setMetadataKeys(md_keys)
 
-        if self.metadata_file != None:
-            self.metadata_file.close()
-            self.metadata_file = None
-
+        # Create per-image metadata file
         time_str = datetime.now().strftime("%Y-%m-%d-%H%M%S")
         self.experiment_folder = time_str + f"_{custom_experiment_name}"
 
@@ -47,14 +58,62 @@ class DataStorage:
         except Exception as e:
             raise DataStorage(e)
 
-        filename = path.join(self.main_dir, self.experiment_folder, time_str) + f"_{custom_experiment_name}"
-        self.metadata_file = open(f"{filename}_metadata.csv", "w")
-        self.md_writer = csv.DictWriter(self.metadata_file, fieldnames=self.md_keys)
+        filename = path.join(self.main_dir, self.experiment_folder, time_str) + f"perimage_{custom_experiment_name}_metadata.csv"
+        self.metadata_file = open(f"{filename}", "w")
+        self.md_writer = csv.DictWriter(self.metadata_file, fieldnames=list(per_image_metadata.keys()))
         self.md_writer.writeheader()
 
+        # Create experiment initialization metadata file
+        exp_run_md_file = path.join(self.main_dir, self.experiment_folder, time_str) + f"exp_{custom_experiment_name}_metadata.csv"
+        with open(f"{exp_run_md_file}", "w") as f:
+            writer = csv.DictWriter(f, fieldnames=list(experiment_initialization_metdata.keys()))
+            writer.writeheader()
+            writer.writerow(experiment_initialization_metdata)
+
+        # Create Zarr Storage
         self.zw.createNewFile(filename)
 
     def writeData(self, image: np.ndarray, metadata: Dict):
+        """Write a new image and its corresponding metadata.
+
+        Paramaeters
+        -----------
+        image: np.ndarray
+            Image to be saved to the Zarr store
+
+        metadata: Dict
+            Dictionary of the per-image metadata to save. Must match the keys that were used to
+            initialize the metadata file in `createNewExperiment(...)`
+        """
+
         if self.zw.writable:
             self.zw.writeSingleArray(image)
             self.md_writer.writerow(metadata)
+
+    def writeSingleImage(self, image: np.ndarray, custom_image_name: str):
+        """Save a single image w/ a custom suffix
+
+        Parameters
+        ----------
+        image: np.ndarray
+            Image to save as a `.png` file
+
+        custom_image_name: str
+            Name to use to save the image (appended at the end of the timestamp)
+        """
+
+        filename = (
+                path.join(self.main_dir, datetime.now().strftime("%Y-%m-%d-%H%M%S"))
+                + f"_{custom_image_name}.png"
+            )
+        cv2.imwrite(filename, image)
+
+    def close(self):
+        """Close the per-image metadata .csv file and Zarr image store"""
+
+        if self.metadata_file != None:
+            self.metadata_file.close()
+            self.metadata_file = None
+
+        if self.zw.writable:
+            self.zw.closeFile()
--- ulc_mm_package/image_processing/processing_constants.py
+++ ulc_mm_package/image_processing/processing_constants.py
@@ -13,3 +13,35 @@ TOL_PERC = 0.1
 
 # ================ Flow rate constants ================ #
 CORRELATION_THRESH = 0.5
+
+# TODO: Determine an actual value for this.
+# You'd work backwards here...how many images do you want of the same cells?
+# Let's say 2 frames/fov of cells. At 33ms/frame, we want each cell to pass
+# through IMG_HEIGHT pixels in 66ms. So IMG_HEIGHT / 66s * 1000ms / 1s = 15.15 heights/second
+TARGET_FLOWRATE = 15 # In units of IMG_HEIGHT pixels/second.
+
+# ================ Data storage constants ================ #
+EXPERIMENT_METADATA_KEYS = [
+        "operator_name",
+        "timestamp",
+        "camera", # AVT / Basler
+        "target_flowrate",
+        "mean_brightness",
+        "percentage_parasitemia", # if known, otherwise none/flag value?
+    ]
+
+PER_IMAGE_METADATA_KEYS = [
+        "im_counter",
+        "measurement_type",
+        "sample_type",
+        "timestamp",
+        "exposure",
+        "motor_pos",
+        "pressure_hpa",
+        "syringe_pos",
+        "flow_control_on",
+        "target_flowrate",
+        "current_flowrate",
+        "autofocus_on",
+        "autofocus_motor_steps",
+    ]
