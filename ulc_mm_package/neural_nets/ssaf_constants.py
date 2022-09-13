e95341b9f471b1b1482df72acaeb8762b5880a0a
i-jey <ilakkiyan.jeyakumar@iJeyakumar-MBP.local>
2022-09-13 11:22:03 -0400

    fix imports, new parameters to set

--- ulc_mm_package/neural_nets/ssaf_constants.py
+++ ulc_mm_package/neural_nets/ssaf_constants.py
@@ -2,3 +2,5 @@
 IMG_HEIGHT = 600    # Change to 772 for AVT, 2x2
 IMG_WIDTH = 800     # Change to 1032 for AVT, 2x2
 AF_PERIOD_S = 10
+AF_BATCH_SIZE = 10
+AUTOFOCUS_MODEL_DIR =  "../neural_nets/autofocus.xml"
