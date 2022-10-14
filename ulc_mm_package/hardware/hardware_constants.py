9e9c905fdf07f00573a4c0d1113f4309a76065c2
paul.lebel <plebel@alumni.stanford.edu>
2022-10-14 14:23:11 -0700

    add profiling dir to gitignore and fix hardware_constants.py sample video selection

--- ulc_mm_package/hardware/hardware_constants.py
+++ ulc_mm_package/hardware/hardware_constants.py
@@ -72,7 +72,7 @@ CAM_FAN_2 = 24
 
 # ================ Simulation constants ================ #
 _viable_videos = ("./sim_media/sample.avi", "./sim_media/sample.mp4")
-VIDEO_PATH = next(vid for vid in _viable_videos if exists(vid))
+VIDEO_PATH = next((vid for vid in _viable_videos if exists(vid)), None)
 VIDEO_REC = "https://drive.google.com/drive/folders/1YL8i5VXeppfIsPQrcgGYKGQF7chupr56"
 
 SIMULATION = False
