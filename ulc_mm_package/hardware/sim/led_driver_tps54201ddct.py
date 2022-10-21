984c70b5216b447fa257ed1f20640d6b1572462b
Axel Jacobsen <axelnj44@gmail.com>
2022-10-21 14:58:55 -0700

    update led_driver sim

--- ulc_mm_package/hardware/sim/led_driver_tps54201ddct.py
+++ ulc_mm_package/hardware/sim/led_driver_tps54201ddct.py
@@ -8,32 +8,31 @@ Purpose: Dummy hardware object simulating LED.
          
 """
 
+from time import sleep
+from ulc_mm_package.hardware.hardware_constants import (
+    LED_PWM_PIN,
+    ANALOG_DIM_MODE_DUTYCYCLE,
+    LED_FREQ,
+)
 from ulc_mm_package.hardware.hardware_constants import LED_FREQ, LED_PWM_PIN
 from ulc_mm_package.hardware.led_driver_tps54201ddct import LEDError
+from ulc_mm_package.hardware.dtoverlay_pwm import PWM_CHANNEL
+
+from ulc_mm_package.hardware.sim.dtoverlay_pwm import dtoverlay_PWM
+
+from ulc_mm_package.hardware.real.led_driver_tps54201ddct import LED_TPS5420TDDCT as RealLED_TPS5420TDDCT
 
 
-class LED_TPS5420TDDCT:
+class LED_TPS5420TDDCT(RealLED_TPS5420TDDCT):
     def __init__(self, pwm_pin: int = LED_PWM_PIN):
         self._isOn = False
-        self.pwm_pin = pwm_pin
         self.pwm_freq = int(LED_FREQ)
-        self.pwm_duty_cycle = 10
+        self.pwm_duty_cycle = ANALOG_DIM_MODE_DUTYCYCLE
+        self.pwm = dtoverlay_PWM(PWM_CHANNEL.PWM2)
 
-    def close(self):
-        self.pwm_duty_cycle = 0
-
-    def _convertPWMValToDutyCyclePerc(self, pwm_val: int) -> float:
-        return pwm_val / 1e6
-
-    def setDutyCycle(self, duty_cycle_perc: float):
-        self.pwm_duty_cycle = self._convertDutyCyclePercentToPWMVal(duty_cycle_perc)
-
-    def _convertDutyCyclePercentToPWMVal(self, duty_cycle_percentage: float) -> int:
-        return int(1e6 * duty_cycle_percentage)
-
-    def turnOn(self):
-        self._isOn = True
-
-    def turnOff(self):
-        self.setDutyCycle(0)
-        self._isOn = False
+        # Set the dimming mode (see datasheet, page 17)
+        self.pwm.setFreq(50000)
+        self.pwm.setDutyCycle(ANALOG_DIM_MODE_DUTYCYCLE)
+        sleep(0.0005)
+        self.pwm.setFreq(self.pwm_freq)
+        self.pwm.setDutyCycle(0)
