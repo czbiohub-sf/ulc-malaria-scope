1766cc52546be43132436eb3ea701e458627e59b
Axel Jacobsen <axelnj44@gmail.com>
2022-10-27 15:48:07 -0700

    remove profiling code

--- ulc_mm_package/QtGUI/scope_op.py
+++ ulc_mm_package/QtGUI/scope_op.py
@@ -5,8 +5,6 @@ Manages hardware routines and interactions with Oracle and Acquisition.
 
 """
 
-import cProfile
-
 import numpy as np
 
 from transitions import Machine
@@ -295,12 +293,7 @@ class ScopeOp(QObject, Machine):
         if self.count >= MAX_FRAMES:
             self.to_intermission()
         else:
-            cProfile.runctx(
-                "count_parasitemia(self.mscope, img, [self.count])",
-                globals(),
-                locals(),
-                "profile.stat",
-            )
+            prev_res = count_parasitemia(self.mscope, img, [self.count])
 
             # Adjust the flow
             try:
