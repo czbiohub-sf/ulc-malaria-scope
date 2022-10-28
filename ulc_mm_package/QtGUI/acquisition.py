0f625cf5f1d80bfb90ad05617069fbd1bf513403
mwlkhoo <michelle.khoo.wl@gmail.com>
2022-10-28 16:50:01 -0700

    Fixed typo

--- ulc_mm_package/QtGUI/acquisition.py
+++ ulc_mm_package/QtGUI/acquisition.py
@@ -16,13 +16,8 @@ from ulc_mm_package.QtGUI.gui_constants import ACQUISITION_PERIOD
 
 class Acquisition(QObject):
     update_liveview = pyqtSignal(np.ndarray)
-<<<<<<< HEAD
-    update_scopeop = pyqtSignal(np.ndarray)
-    
-=======
     update_scopeop = pyqtSignal(np.ndarray, float)
 
->>>>>>> master
     def __init__(self):
         super().__init__()
 
--- ulc_mm_package/QtGUI/gui_constants.py
+++ ulc_mm_package/QtGUI/gui_constants.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import enum
 
 # ================ Status colors ================ #
