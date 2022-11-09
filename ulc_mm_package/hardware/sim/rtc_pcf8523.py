984a80dc21185d0a784ab2993db989601cef632d
mwlkhoo <michelle.khoo.wl@gmail.com>
2022-11-09 12:13:34 -0800

    Added time struct to simulated RTC

--- ulc_mm_package/hardware/sim/rtc_pcf8523.py
+++ ulc_mm_package/hardware/sim/rtc_pcf8523.py
@@ -1,6 +1,6 @@
 import time
 
-from datetime import date
+from datetime import datetime
 
 
 class RTC_PCF8523:
@@ -13,20 +13,7 @@ class RTC_PCF8523:
         pass
 
     def get_time(self):
-        """Get the current time from the RTC
+        datetime_str = datetime.now().strftime("%Y-%m-%d-%H%M%S")
+        time_struct = time.strptime(datetime_str, "%Y-%m-%d-%H%M%S")
 
-        Returns
-        -------
-        time.struct_time object
-            See https://docs.python.org/3/library/time.html#time.struct_time.
-            You can access the following attributes from the returned object:
-            - tm_year (year)
-            - tm_mon (month)
-            - tm_mday (day of the month)
-            - tm_hour
-            - tm_min
-            - tm_sec
-            - tm_wday (day of the week as a number, ranges from [0-6] with Monday being 0)
-            - tm_yday (day of the year, ranges from [1-366])
-        """
-        return "DATETIME HERE"
+        return time_struct
