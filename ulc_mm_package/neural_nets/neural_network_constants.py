757eaa940de71ac385be63318a113f48752448bd
Axel Jacobsen <axelnj44@gmail.com>
2022-11-16 23:59:34 +0000

    fix nn xml file path issue

--- ulc_mm_package/neural_nets/neural_network_constants.py
+++ ulc_mm_package/neural_nets/neural_network_constants.py
@@ -1,9 +1,9 @@
 # ================ Autofocus constants ================ #
 AF_PERIOD_S = 10
 AF_BATCH_SIZE = 10
-AUTOFOCUS_MODEL_DIR = "autofocus_model_files/valiant-disco-119.xml"
+AUTOFOCUS_MODEL_DIR = "../neural_nets/autofocus_model_files/valiant-disco-119.xml"
 
 
 # ================ YOGO constants ================ #
 YOGO_PRED_THRESHOLD = 0.3
-YOGO_MODEL_DIR = "yogo_model_files/yogo.xml"
+YOGO_MODEL_DIR = "../neural_nets/yogo_model_files/yogo.xml"
