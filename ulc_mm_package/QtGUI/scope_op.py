49006cd47c5026497c8150908bc9134bb21d21bd
Axel Jacobsen <axelnj44@gmail.com>
2022-11-25 10:42:09 -0800

    reset scope_op

--- ulc_mm_package/QtGUI/scope_op.py
+++ ulc_mm_package/QtGUI/scope_op.py
@@ -239,7 +239,6 @@ class ScopeOp(QObject, Machine):
         self.mscope.led.turnOff()
 
         closing_file_future = self.mscope.data_storage.close()
-
         while not closing_file_future.done():
             sleep(1)
 
