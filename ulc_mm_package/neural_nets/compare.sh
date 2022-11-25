5c89d6dcd50f00b3e6bf59b2b5823a2ff734191e
Axel Jacobsen <axelnj44@gmail.com>
2022-11-25 16:49:24 +0000

    remove unecessary files

--- ulc_mm_package/neural_nets/compare.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/bash
-
-echo "base infer..."
-(time MS_BATCH="" ./infer_on_steps.sh;) &> base_infer_results.txt
-echo "asyn infer..."
-(time MS_BATCH="asyn_infer" ./infer_on_steps.sh;) &> asyn_infer_results.txt
-echo "no infer..."
-(time MS_BATCH="no_infer" ./infer_on_steps.sh;) &> no_infer_results.txt
-
