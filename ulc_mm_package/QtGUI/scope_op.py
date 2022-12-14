c3982a91d03aad7d454002cc72f70d1ba5214233
Paul Lebel <30475643+paul-lebel@users.noreply.github.com>
2022-12-13 16:12:34 -0800

    Update scope_op.py
    - blackify

--- ulc_mm_package/QtGUI/scope_op.py
+++ ulc_mm_package/QtGUI/scope_op.py
@@ -181,7 +181,7 @@ class ScopeOp(QObject, Machine):
         self.mscope = MalariaScope()
         self.yield_mscope.emit(self.mscope)
         self.mscope.disable_encoder()
-        
+
         component_status = self.mscope.getComponentStatus()
 
         if all([status == True for status in component_status.values()]):
