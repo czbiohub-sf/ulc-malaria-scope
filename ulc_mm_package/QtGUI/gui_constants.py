c641139932ab29d8ca3cc7f03840a6bd7fc32956
i-jey <ilakkiyan.jeyakumar@iJeyakumar-MBP.local>
2022-12-22 13:03:45 -0800

    add biohub as site location

--- ulc_mm_package/QtGUI/gui_constants.py
+++ ulc_mm_package/QtGUI/gui_constants.py
@@ -13,7 +13,7 @@ class STATUS(enum.Enum):
 
 # ================ Dropdown menu options ================ #
 FLOWRATE_LIST = [e.name.capitalize() for e in FLOWRATE]
-SITE_LIST = ["Tororo, Uganda"]
+SITE_LIST = ["Tororo, Uganda", "Biohub SF"]
 
 # ================ FPS constants ================ #
 ACQUISITION_PERIOD = 1000.0 / 30.0
