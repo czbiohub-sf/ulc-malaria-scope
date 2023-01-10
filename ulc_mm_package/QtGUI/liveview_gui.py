f7239ff01ede3f5231ee6870d493fcc3385039bd
Michelle Khoo <michelle.khoo.wl@gmail.com>
2023-01-10 00:15:41 +0000

    Only display sample SSH addr if __main__

--- ulc_mm_package/QtGUI/liveview_gui.py
+++ ulc_mm_package/QtGUI/liveview_gui.py
@@ -238,8 +238,6 @@ class LiveviewGUI(QMainWindow):
         self.update_focus("---")
         self.update_flowrate("---")
 
-        self.update_tcp("Sample address here")
-
         # Setup status colors
         self._set_color(self.state_lbl, STATUS.IN_PROGRESS)
 
@@ -354,7 +352,8 @@ if __name__ == "__main__":
 
     gui.set_infopanel_vals()
 
-    gui.update_msg("Sample message here")
+    gui.update_msg("Sample message here")        
+    gui.update_tcp("Sample address here")
 
     gui.show()
     sys.exit(app.exec_())
