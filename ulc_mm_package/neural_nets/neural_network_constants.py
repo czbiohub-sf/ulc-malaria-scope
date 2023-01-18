e71f09a92d03414a9e13d0fb44bd8ed8f2bddf10
i-jey <ilakkiyan.jeyakumar@iJeyakumar-MBP.local>
2023-01-18 13:35:19 -0800

    slightly more resilient pathing for nn model files

--- ulc_mm_package/neural_nets/neural_network_constants.py
+++ ulc_mm_package/neural_nets/neural_network_constants.py
@@ -1,12 +1,18 @@
+import os
+
+curr_dir = os.path.dirname(os.path.realpath(__file__))
+
 # ================ Autofocus constants ================ #
 AF_PERIOD_S = 10
 AF_BATCH_SIZE = 10
-AUTOFOCUS_MODEL_DIR = "../neural_nets/autofocus_model_files/valiant-disco-119.xml"
+AUTOFOCUS_MODEL_DIR = os.path.join(
+    curr_dir, "autofocus_model_files/valiant-disco-119.xml"
+)
 
 
 # ================ YOGO constants ================ #
 YOGO_PRED_THRESHOLD = 0.3
-YOGO_MODEL_DIR = "../neural_nets/yogo_model_files/glowing-sunset-342.xml"
+YOGO_MODEL_DIR = os.path.join(curr_dir, "yogo_model_files/glowing-sunset-342.xml")
 
 from typing import Tuple, Dict
 
