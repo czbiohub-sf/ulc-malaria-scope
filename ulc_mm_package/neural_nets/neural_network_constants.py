885219ae20645960b27307f61f2b92879db88412
Axel Jacobsen <axelnj44@gmail.com>
2023-01-19 11:02:22 -0800

    Update neural_network_constants.py

--- ulc_mm_package/neural_nets/neural_network_constants.py
+++ ulc_mm_package/neural_nets/neural_network_constants.py
@@ -1,18 +1,17 @@
 import pathlib
+from typing import Tuple, Dict
 
 curr_dir = pathlib.Path(__file__).parent.resolve()  # Get full path
+
 
 # ================ Autofocus constants ================ #
 AF_PERIOD_S = 10
 AF_BATCH_SIZE = 10
 AUTOFOCUS_MODEL_DIR = str(curr_dir / "autofocus_model_files/valiant-disco-119.xml")
 
-
 # ================ YOGO constants ================ #
 YOGO_PRED_THRESHOLD = 0.3
 YOGO_MODEL_DIR = str(curr_dir / "yogo_model_files/glowing-sunset-342.xml")
-
-from typing import Tuple, Dict
 
 YOGO_CLASS_LIST: Tuple[str, ...] = ("healthy", "ring", "schizont", "troph")
 YOGO_CLASS_IDX_MAP: Dict[str, int] = {k: idx for idx, k in enumerate(YOGO_CLASS_LIST)}
