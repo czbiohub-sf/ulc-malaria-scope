fb15ff16133b6a630af756319a8663b967d61872
Axel Jacobsen <axelnj44@gmail.com>
2023-01-24 14:28:06 -0800

    minor celans

--- ulc_mm_package/hardware/multiprocess_scope_routine.py
+++ ulc_mm_package/hardware/multiprocess_scope_routine.py
@@ -13,7 +13,6 @@ from multiprocessing import sharedctypes
 from typing import (
     cast,
     Dict,
-    TypeVar,
     Type,
     Callable,
     Optional,
@@ -102,7 +101,30 @@ class ctypeArrayDefn(NamedTuple):
 ctypeDefn = Union[ctypeValueDefn, ctypeArrayDefn]
 
 
-class SharedCtypeWrapper(abc.ABC):
+_typecode_to_type: Dict[str, Type[_SimpleCData]] = {
+    "c": ctypes.c_char,
+    "u": ctypes.c_wchar,
+    "b": ctypes.c_byte,
+    "B": ctypes.c_ubyte,
+    "h": ctypes.c_short,
+    "H": ctypes.c_ushort,
+    "i": ctypes.c_int,
+    "I": ctypes.c_uint,
+    "l": ctypes.c_long,
+    "L": ctypes.c_ulong,
+    "q": ctypes.c_longlong,
+    "Q": ctypes.c_ulonglong,
+    "f": ctypes.c_float,
+    "d": ctypes.c_double,
+}
+_type_to_typecode = {v: k for k, v in _typecode_to_type.items()}
+
+_ctype_codes = list(_typecode_to_type.keys())
+
+_pytype = Union[Real, npt.NDArray[np.uint8]]
+_ctype_type = Union[Type[_SimpleCData], str]
+
+class SharedctypeWrapper(abc.ABC):
     """
     Wraps shared memory (RawValue / RawArray) in a way
     that is easy to work with for the programmer.
@@ -114,51 +136,32 @@ class SharedCtypeWrapper(abc.ABC):
 
     """
 
-    _typecode_to_type: Dict[str, Type[_SimpleCData]] = {
-        "c": ctypes.c_char,
-        "u": ctypes.c_wchar,
-        "b": ctypes.c_byte,
-        "B": ctypes.c_ubyte,
-        "h": ctypes.c_short,
-        "H": ctypes.c_ushort,
-        "i": ctypes.c_int,
-        "I": ctypes.c_uint,
-        "l": ctypes.c_long,
-        "L": ctypes.c_ulong,
-        "q": ctypes.c_longlong,
-        "Q": ctypes.c_ulonglong,
-        "f": ctypes.c_float,
-        "d": ctypes.c_double,
-    }
-    _type_to_typecode = {v: k for k, v in _typecode_to_type.items()}
-
-    _ctype_codes = list(_typecode_to_type.keys())
-    _pytype = Union[Real, npt.NDArray[np.uint8]]
-
     @classmethod
-    def sharedctype_from_defn(cls, defn: ctypeDefn) -> SharedCtypeWrapper:
+    def sharedctype_from_defn(cls, defn: ctypeDefn) -> SharedctypeWrapper:
         if isinstance(defn, ctypeValueDefn):
-            return SharedCtypeValue.from_definition(defn)
+            return SharedctypeValue.from_definition(defn)
         elif isinstance(defn, ctypeArrayDefn):
-            return SharedCtypeArray.from_definition(defn)
+            return SharedctypeArray.from_definition(defn)
         raise ValueError(f"invalid ctype defin: {defn}")
 
-    def _ctype_defn_to_str(self, type_: Union[Type[_SimpleCData], str]) -> str:
+    def _ctype_defn_to_str(self, type_: _ctype_type) -> str:
         """mypy was complaining a lot, so we have to switch on type(type_) instead
         of doing the more elegant `return _type_to_typecode.get(type_, type_)` which
         has the same type garuntees"""
         if isinstance(type_, str):
+            if not type_ in _ctype_codes:
+                raise ValueError(f"invalid ctype code {type_}")
             return type_
-        return self._type_to_typecode[type_]
+        return _type_to_typecode[type_]
 
     @classmethod
     @abc.abstractmethod
-    def from_definition(cls, defn: ctypeDefn) -> SharedCtypeWrapper:
-        """create SharedCtypeWrapper from the given definition"""
+    def from_definition(cls, defn: ctypeDefn) -> SharedctypeWrapper:
+        """create SharedctypeWrapper from the given definition"""
         ...
 
     @abc.abstractmethod
-    def set(self, v: _pytype):
+    def set(self, v: _pytype) -> None:
         """Set the shared memory to the value of v"""
         ...
 
@@ -169,34 +172,34 @@ class SharedCtypeWrapper(abc.ABC):
         ...
 
 
-class SharedCtypeValue(SharedCtypeWrapper):
+class SharedctypeValue(SharedctypeWrapper):
     def __init__(
-        self, type_: Union[Type[_SimpleCData], str], init_value: Optional[Real]
+        self, type_: _ctype_type, init_value: Optional[Real]
     ):
         self._memory: mp.sharedctypes.Synchronized[Real] = mp.Value(type_, init_value)
 
     @classmethod
-    def from_definition(cls, defn: ctypeDefn) -> SharedCtypeValue:
+    def from_definition(cls, defn: ctypeDefn) -> SharedctypeValue:
         if not isinstance(defn, ctypeValueDefn):
             raise TypeError(
-                f"need ctypeValueDefn to construct SharedCtypeValue - got type {type(defn)}"
+                f"need ctypeValueDefn to construct SharedctypeValue - got type {type(defn)}"
             )
         return cls(defn.type_str, defn.init_value)
 
-    def set(self, v: SharedCtypeWrapper._pytype):
+    def set(self, v: _pytype) -> None:
         if not isinstance(v, Real):
             raise ValueError(f"set value for {self} is of incorrect type {type(v)}")
 
         with self._memory:
             self._memory.value = v
 
-    def get(self) -> SharedCtypeWrapper._pytype:
+    def get(self) -> _pytype:
         with self._memory:
             return self._memory.value
 
 
-class SharedCtypeArray(SharedCtypeWrapper):
-    def __init__(self, type_: Union[Type[_SimpleCData], str], shape: Tuple[int, ...]):
+class SharedctypeArray(SharedctypeWrapper):
+    def __init__(self, type_: _ctype_type, shape: Tuple[int, ...]):
         self._lock = mp.Lock()
 
         size = int(np.prod(shape))
@@ -206,20 +209,21 @@ class SharedCtypeArray(SharedCtypeWrapper):
         self._np_wrapper = np.frombuffer(self._memory, dtype=type_).reshape(shape)
 
     @classmethod
-    def from_definition(cls, defn: ctypeDefn) -> SharedCtypeArray:
+    def from_definition(cls, defn: ctypeDefn) -> SharedctypeArray:
         if not isinstance(defn, ctypeArrayDefn):
             raise TypeError(
-                f"need ctypeArrayDefn to construct SharedCtypeArray - got type {type(defn)}"
+                f"need ctypeArrayDefn to construct SharedctypeArray - got type {type(defn)}"
             )
         return cls(defn.type_str, defn.shape)
 
-    def set(self, v: SharedCtypeWrapper._pytype):
+    def set(self, v: _pytype) -> None:
         if not isinstance(v, np.ndarray):
             raise ValueError(f"set value for {self} is of incorrect type {type(v)}")
 
-        self._np_wrapper[:] = v
+        with self._lock:
+            self._np_wrapper[:] = v
 
-    def get(self) -> SharedCtypeWrapper._pytype:
+    def get(self) -> _pytype:
         return self._np_wrapper
 
 
@@ -229,14 +233,10 @@ class MultiProcFunc:
     def __init__(
         self,
         work_fcn: Callable,
-        work_fn_inputs: List[SharedCtypeWrapper],
-        work_fn_outputs: List[SharedCtypeWrapper],
+        work_fn_inputs: List[SharedctypeWrapper],
+        work_fn_outputs: List[SharedctypeWrapper],
     ):
         self.work_fcn: Callable = work_fcn
-
-        # raw array / value because the input Array and Value are changed together
-        # this may not even be necessary depending on our impl
-        self._value_lock = mp.Lock()
 
         self._input_ctypes = work_fn_inputs
         self._output_ctypes = work_fn_outputs
@@ -258,50 +258,51 @@ class MultiProcFunc:
         work_fcn: Callable,
         work_fn_inputs: List[ctypeDefn],
         work_fn_outputs: List[ctypeDefn],
-    ):
+    ) -> MultiProcFunc:
         # Create the raw shared types for input/output of the function
-        input_vals: List[SharedCtypeWrapper] = [
-            SharedCtypeWrapper.sharedctype_from_defn(inp) for inp in work_fn_inputs
+        input_vals: List[SharedctypeWrapper] = [
+            SharedctypeWrapper.sharedctype_from_defn(inp) for inp in work_fn_inputs
         ]
-        output_vals: List[SharedCtypeWrapper] = [
-            SharedCtypeWrapper.sharedctype_from_defn(out) for out in work_fn_outputs
+        output_vals: List[SharedctypeWrapper] = [
+            SharedctypeWrapper.sharedctype_from_defn(out) for out in work_fn_outputs
         ]
 
         return cls(work_fcn, input_vals, output_vals)
 
     @staticmethod
     def _set_ctypes(
-        set_values: List[SharedCtypeWrapper._pytype], targets: List[SharedCtypeWrapper]
-    ):
+        set_values: List[_pytype], targets: List[SharedctypeWrapper]
+    ) -> None:
         if len(set_values) != len(targets):
             raise ValueError("len(set_values) != len(targets)")
 
         for set_val, target in zip(set_values, targets):
             target.set(set_val)
 
-    def _work(self, input_args, outputs):
+    def _work(
+        self, input_args: List[SharedctypeWrapper], outputs: List[SharedctypeWrapper]
+    ) -> None:
         while True:
             self._new_data_ready.wait()
             self._new_data_ready.clear()
 
-            with self._value_lock:
-                func_args = [inp.get() for inp in self._input_ctypes]
-                ret_vals = self.work_fcn(*func_args)
-                ret_vals = ret_vals if isinstance(ret_vals, tuple) else [ret_vals]
+            func_args = [inp.get() for inp in self._input_ctypes]
+            ret_vals = self.work_fcn(*func_args)
+            ret_vals = ret_vals if isinstance(ret_vals, tuple) else [ret_vals]
 
-                self._set_ctypes(ret_vals, outputs)
+            self._set_ctypes(ret_vals, outputs)
 
             self._ret_value_ready.set()
 
     def call(
-        self, args: List[SharedCtypeWrapper._pytype]
-    ) -> Tuple[SharedCtypeWrapper._pytype, ...]:
-        with self._value_lock:
-            self._set_ctypes(args, self._input_ctypes)
+        self, args: List[_pytype]
+    ) -> Tuple[_pytype, ...]:
+
+        self._set_ctypes(args, self._input_ctypes)
 
         return self._func_call()
 
-    def _func_call(self) -> Tuple[SharedCtypeWrapper._pytype, ...]:
+    def _func_call(self) -> Tuple[_pytype, ...]:
         """
         if self._input_ctypes has been set somewhere else,
         we want to still be able to smoothly call the work func
@@ -311,8 +312,7 @@ class MultiProcFunc:
         self._ret_value_ready.wait()
         self._ret_value_ready.clear()
 
-        with self._value_lock:
-            return tuple(out.get() for out in self._output_ctypes)
+        return tuple(out.get() for out in self._output_ctypes)
 
-    def join(self):
+    def join(self) -> None:
         self._proc.join()
