b0169cc33229e51f3e9a502ff7ea23e3716747ad
Michelle Khoo <michelle.khoo.wl@gmail.com>
2023-01-25 23:14:17 +0000

    Check actual queue

--- ulc_mm_package/QtGUI/scope_op.py
+++ ulc_mm_package/QtGUI/scope_op.py
@@ -749,7 +749,7 @@ class ScopeOp(QObject, NamedMachine):
             zarr_qsize = self.mscope.data_storage.zw.executor._work_queue.qsize()
             self.img_metadata["zarrwriter_qsize"] = zarr_qsize
 
-            ssaf_qsize = self.mscope.autofocus_model._temp_infer_queue.__len__()
+            ssaf_qsize = self.mscope.autofocus_model._executor._work_queue.qsize()
             self._update_metadata_if_verbose("ssaf_qsize", ssaf_qsize)  
 
             self.img_metadata["runtime"] = perf_counter() - current_time
