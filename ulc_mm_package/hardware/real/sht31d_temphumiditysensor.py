abee7064ed55ce97d8e2fa30777bd44c502052fa
Axel Jacobsen <axelnj44@gmail.com>
2023-01-25 10:23:22 -0800

    initial sketches

--- ulc_mm_package/hardware/real/sht31d_temphumiditysensor.py
+++ ulc_mm_package/hardware/real/sht31d_temphumiditysensor.py
@@ -1,14 +1,16 @@
 """ SHT3x-DIS - Humidity and Temperature Sensor
 
--- Important Links -- 
+-- Important Links --
 Datasheet:
     https://media.digikey.com/pdf/Data%20Sheets/Sensirion%20PDFs/HT_DS_SHT3x_DIS.pdf
 Adafruit's PCF8523 Python library:
     https://docs.circuitpython.org/projects/sht31d/en/latest/
 """
 
-import adafruit_sht31d
 import board
+import adafruit_sht31d
+
+from typing import Tuple, List
 
 from ulc_mm_package.hardware.sht31d_temphumiditysensor import (
     TemperatureSensorNotInstantiated,
@@ -25,22 +27,19 @@ class SHT3X:
         i2c = board.I2C()
         try:
             self.sensor = adafruit_sht31d.SHT31D(i2c)
-            self.sensor.mode = adafruit_sht31d.MODE_SINGLE
+            self.sensor.mode = adafruit_sht31d.MODE_PERIODIC
+            self.sensor.frequency = adafruit_sht31d.FREQUENCY_10
         except Exception:
             raise TemperatureSensorNotInstantiated()
 
-    def getRelativeHumidity(self):
-        try:
-            relative_humidity = self.sensor.relative_humidity
-            return relative_humidity
-        except Exception as e:
-            print(f"Error getting relative humidity: {e}")
-            return -1
+    def _get_most_recent_reading(self, readings: List[float]) -> float:
+        """ From the `adafruit_sht31d` package source code:
+        'Periodic' mode returns the most recent readings available from the
+        sensor's cache in a FILO list of eight floats. This list is backfilled
+        with with the sensor's maximum output of 130.0 when the sensor is read
+        before the cache is full.
+        """
 
-    def getTemperature(self):
-        try:
-            temp = self.sensor.temperature
-            return temp
-        except Exception as e:
-            print(f"Error getting temperature: {e}")
-            return -1
+    def get_temp_and_humidity(self) -> Tuple[float, float]:
+        temperature, humidity = self.sensor._read()
+        return temperature, humidity
