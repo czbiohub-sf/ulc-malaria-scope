72957cd75d8419304beb5548bdd236d4c24b1b23
Axel Jacobsen <axelnj44@gmail.com>
2023-01-25 21:13:20 +0000

    fix sim mode

--- ulc_mm_package/hardware/sim/sht31d_temphumiditysensor.py
+++ ulc_mm_package/hardware/sim/sht31d_temphumiditysensor.py
@@ -1,16 +1,8 @@
-""" SHT3x-DIS - Humidity and Temperature Sensor
-
--- Important Links --
-Datasheet:
-    https://media.digikey.com/pdf/Data%20Sheets/Sensirion%20PDFs/HT_DS_SHT3x_DIS.pdf
-Adafruit's PCF8523 Python library:
-    https://docs.circuitpython.org/projects/sht31d/en/latest/
-"""
+from typing import Tuple
 
 
 class SHT3X:
     """Simulated temperature/humidity sensor returning dummy values"""
 
     def get_temp_and_humidity(self) -> Tuple[float, float]:
-        temperature, humidity = self.sensor._read()
         return 25.0, 1.0
