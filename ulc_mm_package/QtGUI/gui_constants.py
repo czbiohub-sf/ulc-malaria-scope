bcce92b37abae9610b90a9970ce09db4f8ab0a44
Axel Jacobsen <axelnj44@gmail.com>
2023-02-01 15:57:28 -0800

    af

--- ulc_mm_package/QtGUI/gui_constants.py
+++ ulc_mm_package/QtGUI/gui_constants.py
@@ -3,6 +3,7 @@ import enum
 from ulc_mm_package.scope_constants import SIMULATION
 from ulc_mm_package.image_processing.processing_constants import FLOWRATE
 
+
 # ================ Status colors ================ #
 class STATUS(enum.Enum):
     DEFAULT = "whitesmoke"
