ac85b4287e0bc18f02a5fb198bdb19b41b4c004b
Axel Jacobsen <axelnj44@gmail.com>
2023-03-07 11:22:50 -0800

    noquote

--- ulc_mm_package/neural_nets/calc_allan_dev.sh
+++ ulc_mm_package/neural_nets/calc_allan_dev.sh
@@ -4,9 +4,9 @@ echo "starting allan dev calcs..."
 
 mkdir -p ~/Documents/autofocus-test-data/results/allan
 
-MS_SIMULATE=1 python3 infer.py --allan-dev --zarr ~/Documents/autofocus-test-data/2022-11-17-112855_medflow_ohmu_focaldrift.zip --output ~/Documents/autofocus-test-data/results/allan/2022-11-17-112855_medflow_ohmu_focaldrift.png --model "autofocus"
+MS_SIMULATE=1 python3 infer.py --allan-dev --zarr ~/Documents/autofocus-test-data/2022-11-17-112855_medflow_ohmu_focaldrift.zip --output ~/Documents/autofocus-test-data/results/allan/2022-11-17-112855_medflow_ohmu_focaldrift.png --model autofocus
 echo "."
-MS_SIMULATE=1 python3 infer.py --allan-dev --zarr ~/Documents/autofocus-test-data/2022-11-17-113250_medflow_ohmu_focaldrift.zip --output ~/Documents/autofocus-test-data/results/allan/2022-11-17-113250_medflow_ohmu_focaldrift.png --model "autofocus"
+MS_SIMULATE=1 python3 infer.py --allan-dev --zarr ~/Documents/autofocus-test-data/2022-11-17-113250_medflow_ohmu_focaldrift.zip --output ~/Documents/autofocus-test-data/results/allan/2022-11-17-113250_medflow_ohmu_focaldrift.png --model autofocus
 echo "."
-MS_SIMULATE=1 python3 infer.py --allan-dev --zarr ~/Documents/autofocus-test-data/2022-11-10-181748_focaldrift.zip --output ~/Documents/autofocus-test-data/results/allan/2022-11-10-181748_focaldrift.png --model "autofocus"
+MS_SIMULATE=1 python3 infer.py --allan-dev --zarr ~/Documents/autofocus-test-data/2022-11-10-181748_focaldrift.zip --output ~/Documents/autofocus-test-data/results/allan/2022-11-10-181748_focaldrift.png --model autofocus
 echo "."
