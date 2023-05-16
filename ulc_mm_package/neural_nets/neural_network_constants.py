b1d3d2d9c199142b9d5a6f3b248f7488d9fed47a
Axel Jacobsen <axelnj44@gmail.com>
2023-05-16 11:36:39 -0700

    update yogo model

--- ulc_mm_package/neural_nets/neural_network_constants.py
+++ ulc_mm_package/neural_nets/neural_network_constants.py
@@ -15,9 +15,9 @@ AUTOFOCUS_MODEL_DIR = str(curr_dir / "autofocus_model_files/valiant-disco-119.xm
 
 # ================ YOGO constants ================ #
 YOGO_PRED_THRESHOLD = 0.3
-YOGO_MODEL_DIR = str(curr_dir / "yogo_model_files/glowing-sunset-342.xml")
+YOGO_MODEL_DIR = str(curr_dir / "yogo_model_files/old-transport-1520.xml")
 
-YOGO_CLASS_LIST: Tuple[str, ...] = ("healthy", "ring", "schizont", "troph")
+YOGO_CLASS_LIST: Tuple[str, ...] = ("healthy", "ring", "schizont", "troph", "gametocyte", "wbc", "misc")
 YOGO_CLASS_IDX_MAP: Dict[str, int] = {k: idx for idx, k in enumerate(YOGO_CLASS_LIST)}
 
 YOGO_PERIOD_S = 0.1
