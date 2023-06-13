9f8bcd8a4fcdcd1253b7b58147a7e390951fbf42
Axel Jacobsen <axelnj44@gmail.com>
2023-06-13 16:28:41 -0700

    update YOGO_PRED_THRESHOLD

--- ulc_mm_package/neural_nets/neural_network_constants.py
+++ ulc_mm_package/neural_nets/neural_network_constants.py
@@ -17,7 +17,7 @@ AF_QSIZE = 10  # For AF_PERIOD_S = 0.5, we have a max delay of 5 sec
 AUTOFOCUS_MODEL_DIR = str(curr_dir / "autofocus_model_files" / "valiant-disco-119.xml")
 
 # ================ YOGO constants ================ #
-YOGO_PRED_THRESHOLD = 0.3
+YOGO_PRED_THRESHOLD = 0.5
 YOGO_MODEL_DIR = str(curr_dir / "yogo_model_files" / "brisk-sweep-9.xml")
 
 YOGO_CLASS_LIST: Tuple[str, ...] = (
