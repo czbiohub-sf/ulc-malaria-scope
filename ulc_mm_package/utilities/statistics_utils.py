3277c8556e29a84d5eae9444f1c5fa4cf3a97cba
i-jey <ilakkiyan.jeyakumar@iJeyakumar-MBP.local>
2023-06-22 14:05:32 -0700

    more explicit variable names + clearer comments for statistics_utils

--- ulc_mm_package/utilities/statistics_utils.py
+++ ulc_mm_package/utilities/statistics_utils.py
@@ -48,28 +48,30 @@ def calc_rms(arr: Union[npt.NDArray, List]) -> float:
 def get_class_stats_str(
     name: str,
     count: npt.NDArray,
-    unsorted_confidences: npt.NDArray,
-    sorted_confidences: npt.NDArray,
+    all_confidences_by_class: npt.NDArray,
+    peak_confidences_by_class: npt.NDArray,
 ) -> str:
     """
     Return results string with statistics for individual class
     """
-    return f"\t{name.upper()}: {int(count)} | {int(np.sum(unsorted_confidences))} ({calc_total_perc_err(sorted_confidences)} | {np.mean(sorted_confidences):.3g} | {np.std(sorted_confidences):.3g})\n"
+    return f"\t{name.upper()}: {int(count)} | {int(np.sum(all_confidences_by_class))} ({calc_total_perc_err(peak_confidences_by_class)} | {np.mean(sorted_confidences):.3g} | {np.std(peak_confidences_by_class):.3g})\n"
 
 
 def get_all_stats_str(
     counts: npt.NDArray,
-    unsorted_confidences: List[npt.NDArray],
-    sorted_confidences: List[npt.NDArray],
+    all_confidences_by_class: List[npt.NDArray],
+    peak_confidences_by_class: List[npt.NDArray],
 ) -> str:
     """
     Parameters
     ----------
     counts: npt.NDArray
-    unsorted_confidences: List[npt.NDArray]
+    all_cnofidences_by_class: List[npt.NDArray]
         List of length NUM_CLASSES, ndarray of size 1 x N (N for however many objects detected in total)
-    sorted_confidences: List[npt.NDArray]
-        List of length NUM_CLASSES, ndarray of size 1 x N (N for however many objects detected in total)
+        This has all the confidences for a given class (including confidences where that class was not the most likely predicted class for a given prediction)
+    peak_confidences_by_class: List[npt.NDArray]
+        List of length NUM_CLASSES, ndarray of size 1 x M (M variable for each class, depends on how many of those class instances were detected)
+        This has all the confidences for objects which were predicted to be a particular class
 
     Returns
     -------
@@ -81,8 +83,8 @@ def get_all_stats_str(
         get_class_stats_str(
             class_name,
             counts[class_idx],
-            unsorted_confidences[class_idx],
-            sorted_confidences[class_idx],
+            all_confidences_by_class[class_idx],
+            peak_confidences_by_class[class_idx],
         )
         for class_name, class_idx in YOGO_CLASS_IDX_MAP.items()
     ]
