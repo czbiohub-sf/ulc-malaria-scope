80239fb0cc7b24cf5cf51ae3897d2d136c3a742c
i-jey <ilakkiyan.jeyakumar@gmail.com>
2023-08-10 19:00:35 -0700

    close the mp pool to prevent memory leak

--- ulc_mm_package/neural_nets/utils.py
+++ ulc_mm_package/neural_nets/utils.py
@@ -261,6 +261,7 @@ def _save_thumbnails_to_disk(
     with mp.Pool() as pool:
         args = [(zarr_store, preds, i, save_dir) for i in range(preds.shape[1])]
         pool.starmap(_write_thumbnail_from_pred_tensor, args)
+        pool.close()
 
 
 def save_parasite_thumbnails_to_disk(
