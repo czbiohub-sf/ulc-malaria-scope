66b73c3db0c3a2f79b18e8f6715bd2b31e558421
Michelle Khoo <michelle.khoo.wl@gmail.com>
2023-10-05 10:57:05 -0700

    Add comment clarifying AF constants

--- ulc_mm_package/neural_nets/neural_network_constants.py
+++ ulc_mm_package/neural_nets/neural_network_constants.py
@@ -8,8 +8,8 @@ curr_dir = pathlib.Path(__file__).parent.resolve()  # Get full path
 
 # ================ Autofocus constants ================ #
 AF_PERIOD_S = 0.5
-AF_PERIOD_NUM = int(AF_PERIOD_S * ACQUISITION_FPS)
-AF_BATCH_SIZE = 10
+AF_PERIOD_NUM = int(AF_PERIOD_S * ACQUISITION_FPS) # Used for periodic (ie. EWMA) autofocus
+AF_BATCH_SIZE = 10 # Used for single shot autofocus
 
 AF_THRESHOLD = 2
 AF_QSIZE = 10  # For AF_PERIOD_S = 0.5, we have a max delay of 5 sec
