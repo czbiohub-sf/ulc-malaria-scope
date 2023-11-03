2f1cd28254aa62cab3e0f951de12fe77304babd9
Axel Jacobsen <axelnj44@gmail.com>
2023-11-03 14:53:00 -0700

    upd to frightful wendigoooo

--- ulc_mm_package/neural_nets/neural_network_constants.py
+++ ulc_mm_package/neural_nets/neural_network_constants.py
@@ -23,7 +23,7 @@ AUTOFOCUS_MODEL_DIR = str(
 # ================ YOGO constants ================ #
 YOGO_PRED_THRESHOLD = 0.5
 YOGO_MODEL_DIR = str(
-    curr_dir / "yogo_model_files" / "fine-voice-1816" / "fine-voice-1816-quarter.xml"
+    curr_dir / "yogo_model_files" / "frightful-wendigo-1916" / "frightful-wendigo-1916-quarter.xml"
 )
 
 YOGO_CLASS_LIST: Tuple[str, ...] = (
