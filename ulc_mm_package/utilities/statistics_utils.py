Correct computations to prevent int overflow

ulc_mm_package/utilities/statistics_utils.py
@@ -10,8 +10,10 @@ from ulc_mm_package.neural_nets.neural_network_constants import (
     ASEXUAL_PARASITE_CLASS_IDS,
 )
 
-
 import pandas as pd
+
+# TODO dummy define above constants to run in ipython
+# TODO rewrite supplementary note in more detail
 
 class StatsUtils():
     def __init__(self):
@@ -79,8 +81,10 @@ class StatsUtils():
         """
         Return relative uncertainty of each class count based on deskewing and Poisson statistics
         """
+        # TODO clamp this above 0?
+
         rel_poisson_errs = self.calc_rel_poisson_errs(deskewed_counts)
-        rel_deskew_errs = self.calc_rel_deskew_errs(raw_counts)
+        rel_deskew_errs = self.calc_rel_deskew_errs(raw_counts, deskewed_counts)
 
         print(f"RAW: {raw_counts[0]}")
         print(f"DESKEWED: {deskew_counts[0]}")
@@ -98,24 +102,30 @@ class StatsUtils():
         sqrt_counts = np.sqrt(deskewed_counts)
 
         return np.divide(
-                            1, 
-                            sqrt_counts, 
-                            out=np.zeros(sqrt_counts.shape, dtype=deskewed_counts.dtype),
-                            where=~np.isclose(sqrt_counts, 0)
-                        )
+                    1, 
+                    sqrt_counts, 
+                    out=np.zeros(sqrt_counts.shape, dtype=deskewed_counts.dtype),
+                    where=~np.isclose(sqrt_counts, 0)
+                )
 
 
-    def calc_rel_deskew_errs(self, raw_counts: npt.NDArray) -> npt.NDArray:
+    def calc_rel_deskew_errs(self, raw_counts: npt.NDArray, deskewed_counts: npt.NDArray) -> npt.NDArray:
         """
         Return absolute uncertainty of each class count based on deskewing only
         """
 
         # TODO how to deal with this square??
+        divided_deskewed_counts = np.divide(
+                                1, 
+                                deskewed_counts, 
+                                out=np.zeros(deskewed_counts.shape, dtype=deskewed_counts.dtype),
+                                where=~np.isclose(deskewed_counts, 0)
+                            )
+                            
+        class_ratios = np.outer(divided_deskewed_counts, raw_counts)
+        product = np.matmul(np.square(class_ratios), np.square(self.inv_cmatrix_std))
 
-        squared_raw_counts = np.square(raw_counts)
-        squared_inv_cmatrix_std = np.square(self.inv_cmatrix_std)
-
-        return np.matmul(squared_raw_counts, squared_inv_cmatrix_std)
+        return np.diagonal(product)
 
 
     def get_class_stats_str(