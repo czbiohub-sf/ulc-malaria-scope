a14f852dc9c799ae25e220d64614625778da1910
Michelle Khoo <michelle.khoo@czbiohub.org>
2024-01-09 16:57:55 -0800

    Fixed stats based on expansion of deskewing

--- ulc_mm_package/utilities/statistics_utils.py
+++ ulc_mm_package/utilities/statistics_utils.py
@@ -2,7 +2,6 @@ import numpy as np
 import numpy.typing as npt
 
 from typing import List, Union
-from math import sqrt
 
 from ulc_mm_package.neural_nets.neural_network_constants import (
     YOGO_CLASS_IDX_MAP,
@@ -50,66 +49,60 @@ class StatsUtils():
         return parasites / RBCs
 
             
-    def calc_parasitemia_rel_err(self, rel_errs: npt.NDArray, deskewed_counts: npt.NDArray) -> float:
+    def calc_parasitemia_rel_err(self, count_errs: npt.NDArray, deskewed_counts: npt.NDArray) -> float:
         """
         Return relative uncertainty of total parasitemia count
+        See remoscope manuscript for full derivation
         """
         # Filter for parasite classes only
-        parasite_rel_errs = rel_errs[ASEXUAL_PARASITE_CLASS_IDS]
+        parasite_count_errs = count_errs[ASEXUAL_PARASITE_CLASS_IDS]
+        parasite_count = np.sum(deskewed_counts[ASEXUAL_PARASITE_CLASS_IDS])
 
         # Compute error
-        parasitemia_abs_err = sqrt(np.sum(np.square(parasite_rel_errs)))
-        parasitemia_rel_err = parasitemia_abs_err / self.calc_parasitemia(deskewed_counts)
-        return parasitemia_rel_err
+        return np.sqrt(np.sum(np.square(parasite_count_errs))) / parasite_count
 
 
-    def calc_class_rel_errs(self, raw_counts: npt.NDArray, deskewed_counts: npt.NDArray) -> npt.NDArray:
+    def calc_class_count_errs(self, raw_counts: npt.NDArray, deskewed_counts: npt.NDArray) -> npt.NDArray:
         """
-        Return relative uncertainty of each class count based on deskewing and Poisson statistics
+        Return absolute uncertainty of each class count based on deskewing and Poisson statistics
+        See remoscope manuscript for full derivation
         """
-        rel_poisson_errs = self.calc_rel_poisson_errs(deskewed_counts)
-        rel_deskew_errs = self.calc_rel_deskew_errs(raw_counts, deskewed_counts)
+        poisson_terms = self.calc_poisson_count_err_term(raw_counts)
+        deskew_terms = self.calc_deskew_count_err_terms(raw_counts)
 
-        rel_errs = np.sqrt(np.square(rel_poisson_errs) + np.square(rel_deskew_errs))
+        errs = np.sqrt(poisson_errs + deskew_errs)
 
-        print(f"POISSON {rel_poisson_errs[ASEXUAL_PARASITE_CLASS_IDS]}")
-        print(f"DESKEW {rel_deskew_errs[ASEXUAL_PARASITE_CLASS_IDS]}")
-        print(f"REL {rel_errs[ASEXUAL_PARASITE_CLASS_IDS]}")
+        print(f"POISSON {poisson_terms[ASEXUAL_PARASITE_CLASS_IDS]}")
+        print(f"DESKEW {deskew_terms[ASEXUAL_PARASITE_CLASS_IDS]}")
+        print(f"REL {errs[ASEXUAL_PARASITE_CLASS_IDS]}")
 
-        return rel_errs
+        return errs
 
 
-    def calc_rel_poisson_errs(self, deskewed_counts: npt.NDArray) -> npt.NDArray:
+    def calc_poisson_count_err_term(self, raw_counts: npt.NDArray) -> npt.NDArray:
         """
-        Return relative uncertainty of each class count based on Poisson statistics only
-        NOTE: Relative uncertainty is scaled by sum of parasite classes
+        Return absolute uncertainty term of each class count based on Poisson statistics
+        See remoscope manuscript for full derivation
         """
-        parasite_count = np.sum(deskewed_counts[ASEXUAL_PARASITE_CLASS_IDS])
-
-        # if parasite_count < 1:
-        #     return np.zeros(self.matrix_dim)
-
-        return np.sqrt(deskewed_counts) / parasite_count
+        return np.matmul(raw_counts, np.square(self.inv_cmatrix))
 
 
-    def calc_rel_deskew_errs(self, raw_counts: npt.NDArray, deskewed_counts: npt.NDArray) -> npt.NDArray:
+    def calc_deskew_count_err_terms(self, raw_counts: npt.NDArray) -> npt.NDArray:
         """
-        Return relative uncertainty of each class count based on deskewing only
-        NOTE: Relative uncertainty is scaled by sum of parasite classes
+        Return absolute uncertainty term of each class count based on deskewing
+        See remoscope manuscript for full derivation
         """
-        parasite_count = np.sum(deskewed_counts[ASEXUAL_PARASITE_CLASS_IDS])
-        RBC_count = np.sum(deskewed_counts[RBC_CLASS_IDS])
+        # Commented out for now because int overflow should not be an issue with bug fixes
+        # # TODO does division by 0 cause error?
+        # RBC_count = np.sum(raw_counts[RBC_CLASS_IDS])
 
-        # if parasite_count < 0.001:
-        #     return np.zeros(self.matrix_dim)
+        # # Use ratio of class relative to RBC count to avoid overflow  
+        # class_ratios = raw_counts / RBC_count
+        # unscaled_err = np.matmul(np.square(class_ratios), np.square(self.inv_cmatrix_std))
 
-        # Use ratio of class relative to RBC count to avoid overflow  
-        class_ratios = raw_counts / RBC_count
-        unscaled_err = np.matmul(np.square(class_ratios), np.square(self.inv_cmatrix_std))
+        # return unscaled_err * RBC_count **2 
 
-        # TODO test this
-        # Replace scaling with parasite count instead of RBC count
-        return unscaled_err * RBC_count **2 / parasite_count **2
+        return np.matmul(np.square(raw_counts), np.square(self.inv_cmatrix_std))
 
 
     def get_class_stats_str(
@@ -143,7 +136,7 @@ class StatsUtils():
         deskewed_counts = self.calc_deskewed_counts(raw_counts, int_out=False)
         
         # Get uncertainty
-        rel_errs = self.calc_class_rel_errs(raw_counts, deskewed_counts)
+        rel_errs = self.calc_class_count_errs(raw_counts, deskewed_counts)
         percent_errs = np.multiply(rel_errs, 100)
 
         template_string = "Class results: Count (%% uncertainty)\n"
