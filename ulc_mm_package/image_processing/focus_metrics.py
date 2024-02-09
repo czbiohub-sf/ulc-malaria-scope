a644d1f01075d8fbe873f140193139c1d7ec6409
i-jey <ilakkiyan.jeyakumar@gmail.com>
2024-02-08 23:43:09 -0800

    Add numba-compatible hand-written np.gradient

--- ulc_mm_package/image_processing/focus_metrics.py
+++ ulc_mm_package/image_processing/focus_metrics.py
@@ -1,3 +1,4 @@
+from numba import njit
 import numpy as np
 import cv2
 
@@ -21,7 +22,7 @@ def logPowerSpectrumRadialAverageSum(img):
     return np.sum(radial_average(log_ps))
 
 
-def gradientAverage(img):
+def gradientAverage(img: np.ndarray):
     """Returns the normalized gradient average (in x and y)"""
     gx, gy = np.gradient(img) / np.max(img)
     return np.average(np.sqrt(gx**2 + gy**2))
@@ -29,3 +30,51 @@ def gradientAverage(img):
 
 def varianceOfLaplacian(img):
     return cv2.Laplacian(img, cv2.CV_64F).var()
+
+
+@njit
+def get_diff(img: np.ndarray):
+    img = np.asarray(img, dtype=np.float32)
+    gx = np.zeros_like(img)
+    gy = np.zeros_like(img)
+    rows, cols = img.shape
+
+    # Top left edge
+    gx[0, 0] = img[0, 1] - img[0, 0]
+    gy[0, 0] = img[1, 0] - img[0, 0]
+
+    # Top right edge
+    gx[0, -1] = img[0, -1] - img[0, -2]
+    gy[0, -1] = img[1, -1] - img[0, -1]
+
+    # Bottom left edge
+    gx[-1, 0] = img[-1, 1] - img[-1, 0]
+    gy[-1, 0] = img[-1, 0] - img[-2, 0]
+
+    # Bottom right edge
+    gx[-1, -1] = img[-1, -1] - img[-1, -2]
+    gy[-1, -1] = img[-1, -1] - img[-2, -1]
+
+    # Top/bottom row
+    for i in range(1, cols - 1):
+        gx[0, i] = (img[0, i + 1] - img[0, i - 1]) / 2
+        gy[0, i] = img[1, i] - img[0, i]
+
+        gx[-1, i] = (img[-1, i + 1] - img[-1, i - 1]) / 2
+        gy[-1, i] = img[-1, i] - img[-2, i]
+
+    # First/last columns
+    for j in range(1, rows - 1):
+        gx[j, 0] = img[j, 1] - img[j, 0]
+        gy[j, 0] = (img[j + 1, 0] - img[j - 1, 0]) / 2
+
+        gx[j, -1] = img[j, -1] - img[j, -2]
+        gy[j, -1] = (img[j + 1, -1] - img[j - 1, -1]) / 2
+
+    # Inner
+    for i in range(1, rows - 1):
+        for j in range(1, cols - 1):
+            gx[i, j] = (img[i, j + 1] - img[i, j - 1]) / 2
+            gy[i, j] = (img[i + 1, j] - img[i - 1, j]) / 2
+
+    return gy, gx
