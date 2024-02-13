312f8b957ea534bb63a7b998a7de185360c968d1
i-jey <ilakkiyan.jeyakumar@gmail.com>
2024-02-13 09:51:05 -0800

    set ht/wt based on camera selection

--- ulc_mm_package/image_processing/flowrate.py
+++ ulc_mm_package/image_processing/flowrate.py
@@ -6,19 +6,18 @@ import numpy as np
 from typing import Optional
 
 from ulc_mm_package.hardware import multiprocess_scope_routine as msr
-
+from ulc_mm_package.scope_constants import CAMERA_SELECTION, DOWNSAMPLE_FACTOR
 
 class FlowRateEstimatorError(Exception):
     """Base class for catching all pressure control related errors."""
 
     pass
 
-
 class FlowRateEstimator:
     def __init__(
         self,
-        img_height: int = 77,
-        img_width: int = 103,
+        img_height: int = CAMERA_SELECTION.IMG_HEIGHT // DOWNSAMPLE_FACTOR,
+        img_width: int = CAMERA_SELECTION.IMG_WIDTH // DOWNSAMPLE_FACTOR,
         scale_factor: int = 10,
     ):
         """A class for estimating the flow rate of cells using a 2D cross-correlation.
