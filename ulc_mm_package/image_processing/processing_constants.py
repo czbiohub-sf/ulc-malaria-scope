58bde77aeb5577f9040f1426659e9cc2a74fb0e3
i-jey <ilakkiyan.jeyakumar@gmail.com>
2024-09-16 15:41:30 -0700

    reduce autobrightness pid frequency to once every two seconds (down from once every second)

--- ulc_mm_package/image_processing/processing_constants.py
+++ ulc_mm_package/image_processing/processing_constants.py
@@ -17,7 +17,7 @@ TOP_PERC_TARGET_VAL = 235
 TOP_PERC = 0.03
 TOL = 0.01
 MIN_ACCEPTABLE_MEAN_BRIGHTNESS = 200
-PERIODIC_AB_PERIOD_NUM_FRAMES = 30  # At 30 frames, that's roughly once per second
+PERIODIC_AB_PERIOD_NUM_FRAMES = 60  # At 60 frames, that's roughly once per 2 seconds
 
 # Autobrightness PID constants
 AB_PID_KP = 0.001
