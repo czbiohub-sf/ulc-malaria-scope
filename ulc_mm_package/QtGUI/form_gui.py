fc2caebf287ddd4039c6123cef530942dc28e6f5
Michelle Khoo <michelle.khoo.wl@gmail.com>
2024-10-16 11:19:20 -0700

    Remove unnecessary import

--- ulc_mm_package/QtGUI/form_gui.py
+++ ulc_mm_package/QtGUI/form_gui.py
@@ -19,7 +19,7 @@ from PyQt5.QtWidgets import (
     QDesktopWidget,
 )
 from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSignal, Qt
+from PyQt5.QtCore import pyqtSignal
 
 from ulc_mm_package.scope_constants import EXPERIMENT_METADATA_KEYS
 from ulc_mm_package.image_processing.processing_constants import TARGET_FLOWRATE
