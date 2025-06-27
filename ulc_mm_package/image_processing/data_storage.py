2c80e5c556eb12340a1d990198e117845146f434
i-jey <ilakkiyan.jeyakumar@gmail.com>
2025-06-27 15:35:50 -0700

    remove unused class var in data_storage

--- ulc_mm_package/image_processing/data_storage.py
+++ ulc_mm_package/image_processing/data_storage.py
@@ -136,9 +136,6 @@ class DataStorage:
         # mypy
         assert self.main_dir is not None
 
-        # Reset subsample folder
-        self.sub_seq_path = None
-
         # Create per-image metadata file
         self.time_str = datetime.now().strftime(DATETIME_FORMAT)
         self.experiment_folder = self.time_str + f"_{custom_experiment_name}"
@@ -480,7 +477,6 @@ class DataStorage:
 
         try:
             sub_seq_path = self._create_subseq_folder()
-            self.sub_seq_path = Path(sub_seq_path)
         except:
             self.logger.error(
                 "Unable to create the subsample folder. Aborting subsampling."
